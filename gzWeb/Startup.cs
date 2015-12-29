927203763622e2900dc7c1d0f3e97221f38ff54d
Mario Karagiorgas <salem8@gmail.com>
2015-12-30 00:21:53 +0200

    ADD: first version of Greenzorro solution

---
+++ gzWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gzWeb.Startup))]
+namespace gzWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
