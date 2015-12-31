8a129d26b818926324cf0b94795a229c58f0971e
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2015-12-31 17:46:56 +0200

    ADD: customer profile Poco ent CHANGE: default db connection

---
+++ gzWeb/Models/CustomerProfile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace gzWeb.Models
+{
+    public partial class ApplicationUser : IdentityUser
+    {
+        public virtual CustomerProfile CustomerProfile { get; set; }
+    }
+
+    public class CustomerProfile
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DoB { get; set; }
+    }
+
+    public class ApplicationDbObject : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbObject()
+            : base("DefaultConnection")
+        { }
+
+        public DbSet<CustomerProfile> CustomerProfile { get; set; }
+    }
+}
