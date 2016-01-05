843a58fd1de5d9e75631594a02974963ba70e708
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2016-01-06 00:35:07 +0200

    FIX: Add navigation properties to weights

--- gzWeb/Models/Portfolio.cs
+++ gzWeb/Models/Portfolio.cs
@@ -22,6 +22,7 @@ namespace gzWeb.Models {
         [StringLength(128)]
         public string Note { get; set; }
 
+        public virtual ICollection<PortFunds> FundWeights { get; set; }
         public virtual ICollection<Fund> Funds { get; set; }
     }
 }
