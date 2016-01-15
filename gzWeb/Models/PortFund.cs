1a41842fabee347af3f8c9d79ef1741e979881c9
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2016-01-15 11:33:16 +0200

    CHANGE: Correct Portfolio to Funds relationship, qry for balance.

--- gzWeb/Models/PortFund.cs
+++ gzWeb/Models/PortFund.cs
@@ -6,19 +6,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace gzWeb.Models {
     /// <summary>
     /// Bridge for Many to Many Portfolio <--> Fund
+    /// Many to Many with additional fields in association table
+    /// http://stackoverflow.com/questions/7050404/create-code-first-many-to-many-with-additional-fields-in-association-table
     /// </summary>
     public class PortFund {
 
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required, Key, Column(Order = 0)]
+        public int PortfolioId { get; set; }
+
+        [Required, Key, Column(Order = 1)]
+        public int FundId { get; set; }
+
+        public virtual Portfolio Portfolio { get; set; }
+        public virtual Fund Fund { get; set; }
 
         [Required]
         public float Weight { get; set; }
-
-        [Required, Index]
-        public int PortfolioId { get; set; }
-
-        [Required]
-        public int FundId { get; set; }
     }
 }
