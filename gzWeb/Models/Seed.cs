e9bc22030c96cd1a35bf2e84748b528a0ef46a04
Mario Karagiorgas <salem8@gmail.com>
2016-01-15 14:56:09 +0200

    CHANGE: Move Seed method to static method in models ADD: test method for seed.. but not working... no transxtype returned.

--- gzWeb/Models/Seed.cs
+++ gzWeb/Models/Seed.cs
@@ -1,153 +1,84 @@
-namespace gzWeb.Migrations {
-    using Microsoft.AspNet.Identity;
-    using Models;
-    using System;
-    using System.Data.Entity.Migrations;
-    using Microsoft.AspNet.Identity.Owin;
-    using System.Data.Entity;
-    using System.Linq;
-    using System.IO;
-    internal sealed class Configuration : DbMigrationsConfiguration<gzWeb.Models.ApplicationDbContext> {
-        public Configuration() {
-            AutomaticMigrationsEnabled = false;
-        }
+﻿using System;
+using System.Data.Entity.Migrations;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNet.Identity;
 
-        protected override void Seed(gzWeb.Models.ApplicationDbContext context) {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+namespace gzWeb.Models {
+    public class Seed {
+        public static string GenData() {
+            //Assume success
+            string retVal = null;
 
             //http://stackoverflow.com/questions/815586/entity-framework-using-transactions-or-savechangesfalse-and-acceptallchanges
-            //using (var sqlLogFile = new StreamWriter("d:\\temp\\sqlLogFile.txt")) {
-            //    context.Database.Log = sqlLogFile.Write;
-            //    using (var dbContextTransaction = context.Database.BeginTransaction()) {
-            //        try {
+            using (var sqlLogFile = new StreamWriter("d:\\temp\\sqlLogFile.txt")) {
+                using (ApplicationDbContext context = new ApplicationDbContext()) {
+                    context.Database.Log = sqlLogFile.Write;
+                    using (var dbContextTransaction = context.Database.BeginTransaction()) {
+                        try {
 
-            //            AddUpdData(context);
-            //            context.SaveChanges();
+                            AddUpdData(context);
+                            context.SaveChanges();
 
-            //            dbContextTransaction.Commit();
-            //        } catch (Exception ex) {
-            //            var exception = ex.Message;
-            //            dbContextTransaction.Rollback();
-            //        }
-            //    }
-            //    base.Seed(context);
-            //}
-            Models.Seed.GenData();
+                            dbContextTransaction.Commit();
+                        } catch (Exception ex) {
+                            var exception = retVal = ex.Message;
+                            dbContextTransaction.Rollback();
+                        }
+                    }
+                }
+            }
+            return retVal;
         }
 
         private static void AddUpdData(ApplicationDbContext context) {
             var manager = new ApplicationUserManager(new CustomUserStore(context));
 
-            // User
-            Random rnd = new Random();
-            int rndPlatformId = rnd.Next(1, int.MaxValue);
-
-            var newUser = new ApplicationUser() {
-                UserName = "joe@mymail.com",
-                Email = "joe@mymail.com",
-                EmailConfirmed = true,
-                FirstName = "Joe",
-                LastName = "Smith",
-                Birthday = new DateTime(1990, 1, 1),
-                PlatformCustomerId = rndPlatformId,
-                GamBalance = new decimal(4200.54),
-                GamBalanceUpdOnUTC = DateTime.Now
-            };
-
-            // Truncate tables
-            //http://stackoverflow.com/questions/24209220/ef6-code-first-drop-tables-not-entire-database-when-model-changes
-
-            var fUser = manager.FindByEmail(newUser.Email);
-            if (fUser == null) {
-                manager.Create(newUser, "1q2w3e");
-            } else {
-                manager.Update(newUser);
-            }
-
-            var custId = manager.FindByEmail(newUser.Email).Id;
+            // Customer
+            int custId = CreateUpdUser(manager);
 
             // Funds
-            context.Funds.AddOrUpdate(
-                f => f.Symbol,
-                new Fund {
-                    HoldingName = "iShares MUB", Symbol = "MUB"
-                },
-                new Fund {
-                    HoldingName = "Schwab SCHP", Symbol = "SCHP"
-                },
-                new Fund {
-                    HoldingName = "State Street XLE", Symbol = "XLE"
-                },
-                new Fund {
-                    HoldingName = "Vanguard VEA", Symbol = "VEA"
-                },
-                new Fund {
-                    HoldingName = "Vanguard VIG", Symbol = "VIG"
-                },
-                new Fund {
-                    HoldingName = "Vanguard VTI", Symbol = "VTI"
-                },
-                new Fund {
-                    HoldingName = "Vanguard VWO", Symbol = "VWO"
-                }
-                );
-
-            #region TransxTypes
+            CreateUpdFunds(context);
 
             // TransxTypes
-
-            context.TransxTypes.AddOrUpdate(
-                t => t.Code,
-                new TransxType {
-                    Code = TransferTypeEnum.Deposit,
-                    Description = "Customer Bank Deposit"
-                },
-                new TransxType {
-                    Code = TransferTypeEnum.Withdrawal,
-                    Description = "Customer withdrawals of any excess funds to their account"
-                },
-                new TransxType {
-                    Code = TransferTypeEnum.TransferToGaming,
-                    Description = "Customer transfers to gaming account"
-                },
-                new TransxType {
-                    Code = TransferTypeEnum.PlayingLoss,
-                    Description = "Losses due to playing in Casino, Betting etc"
-                },
-                new TransxType {
-                    Code = TransferTypeEnum.CreditedPlayingLoss,
-                    Description = "Losses credited to players account after a 50% deduction"
-                },
-                new TransxType {
-                    Code = TransferTypeEnum.InvestmentRet,
-                    Description = "Any gains by the investment returns"
-                },
-                new TransxType {
-                    Code = TransferTypeEnum.FundFee,
-                    Description = "Any fees by the Fund itself i.e. 0.5%"
-                },
-                new TransxType {
-                    Code = TransferTypeEnum.Commission,
-                    Description = "Commissions (1.5% = 2.5%) Profit for GreenZorro"
-                }
-                );
-
-            #endregion
-
-            #region Transxes
+            CreateUpdTranxType(context);
 
             // Transxs
+            CreateUpdTransx(context, custId);
+
+            // Balances
+            CreateBalances(context, custId);
+        }
+
+        private static void CreateBalances(ApplicationDbContext context, int custId) {
+            var yearMonthsGroups = context.Transxes.Where(t => t.CustomerId == custId)
+                .OrderBy(t => t.YearMonthCtd)
+                .GroupBy(t => t.YearMonthCtd)
+                .ToList();
+            //yearMonths.Dump();
+            var prevMonBal = new decimal(0.00);
+            foreach (var g in yearMonthsGroups) {
+
+                var InvAmount = g.Sum(t => t.Type.Code == TransferTypeEnum.CreditedPlayingLoss ? t.Amount : 0);
+                var InvGain = g.Sum(t => t.Type.Code == TransferTypeEnum.InvestmentRet ? t.Amount : 0);
+                var WithdrawnAmounts = g.Sum(t => t.Type.Code == TransferTypeEnum.Withdrawal || t.Type.Code == TransferTypeEnum.TransferToGaming ? t.Amount : 0);
+
+                var gBalance = prevMonBal + InvAmount + InvGain - WithdrawnAmounts;
+
+                context.InvBalances.AddOrUpdate(
+                    b => new { b.CustomerId, b.YearMonthCtd },
+                    new InvBalance {
+                        Balance = gBalance,
+                        CustomerId = custId,
+                        YearMonthCtd = g.Key
+                    }
+                );
+                //Now this is the previous month balance
+                prevMonBal = gBalance;
+            }
+        }
+
+        private static void CreateUpdTransx(ApplicationDbContext context, int custId) {
             context.Transxes.AddOrUpdate(
                 t => new { t.CustomerId, t.CreatedOnUTC, t.TypeId },
                 //March
@@ -327,34 +258,102 @@ namespace gzWeb.Migrations {
                     Type = context.TransxTypes.Where(t => t.Code == TransferTypeEnum.CreditedPlayingLoss).FirstOrDefault()
                 }
                 );
+        }
 
-            #endregion
-
-            var yearMonthsGroups = context.Transxes.Where(t => t.CustomerId == custId)
-                .OrderBy(t => t.YearMonthCtd)
-                .GroupBy(t => t.YearMonthCtd)
-                .ToList();
-            //yearMonths.Dump();
-            var prevMonBal = new decimal(0.00);
-            foreach (var g in yearMonthsGroups) {
-
-                var InvAmount = g.Sum(t => t.Type.Code == TransferTypeEnum.CreditedPlayingLoss ? t.Amount : 0);
-                var InvGain = g.Sum(t => t.Type.Code == TransferTypeEnum.InvestmentRet ? t.Amount : 0);
-                var WithdrawnAmounts = g.Sum(t => t.Type.Code == TransferTypeEnum.Withdrawal || t.Type.Code == TransferTypeEnum.TransferToGaming ? t.Amount : 0);
-
-                var gBalance = prevMonBal + InvAmount + InvGain - WithdrawnAmounts;
-
-                context.InvBalances.AddOrUpdate(
-                    b => new { b.CustomerId, b.YearMonthCtd },
-                    new InvBalance {
-                        Balance = gBalance,
-                        CustomerId = custId,
-                        YearMonthCtd = g.Key
-                    }
+        private static void CreateUpdTranxType(ApplicationDbContext context) {
+            context.TransxTypes.AddOrUpdate(
+                t => t.Code,
+                new TransxType {
+                    Code = TransferTypeEnum.Deposit,
+                    Description = "Customer Bank Deposit"
+                },
+                new TransxType {
+                    Code = TransferTypeEnum.Withdrawal,
+                    Description = "Customer withdrawals of any excess funds to their banking account"
+                },
+                new TransxType {
+                    Code = TransferTypeEnum.TransferToGaming,
+                    Description = "Customer transfers to gaming account"
+                },
+                new TransxType {
+                    Code = TransferTypeEnum.PlayingLoss,
+                    Description = "Losses due to playing in Casino, Betting etc"
+                },
+                new TransxType {
+                    Code = TransferTypeEnum.CreditedPlayingLoss,
+                    Description = "Losses credited to players account after a 50% deduction"
+                },
+                new TransxType {
+                    Code = TransferTypeEnum.InvestmentRet,
+                    Description = "Any gains by the investment returns"
+                },
+                new TransxType {
+                    Code = TransferTypeEnum.FundFee,
+                    Description = "Any fees by the Fund itself i.e. 0.5%"
+                },
+                new TransxType {
+                    Code = TransferTypeEnum.Commission,
+                    Description = "Commissions (1.5% = 2.5%) Profit for GreenZorro"
+                }
                 );
-                //Now this is the previous month balance
-                prevMonBal = gBalance;
+        }
+
+        private static void CreateUpdFunds(ApplicationDbContext context) {
+            context.Funds.AddOrUpdate(
+                f => f.Symbol,
+                new Fund {
+                    HoldingName = "iShares MUB", Symbol = "MUB"
+                },
+                new Fund {
+                    HoldingName = "Schwab SCHP", Symbol = "SCHP"
+                },
+                new Fund {
+                    HoldingName = "State Street XLE", Symbol = "XLE"
+                },
+                new Fund {
+                    HoldingName = "Vanguard VEA", Symbol = "VEA"
+                },
+                new Fund {
+                    HoldingName = "Vanguard VIG", Symbol = "VIG"
+                },
+                new Fund {
+                    HoldingName = "Vanguard VTI", Symbol = "VTI"
+                },
+                new Fund {
+                    HoldingName = "Vanguard VWO", Symbol = "VWO"
+                }
+                );
+        }
+
+        private static int CreateUpdUser(ApplicationUserManager manager) {
+            // User
+            Random rnd = new Random();
+            int rndPlatformId = rnd.Next(1, int.MaxValue);
+
+            var newUser = new ApplicationUser() {
+                UserName = "joe@mymail.com",
+                Email = "joe@mymail.com",
+                EmailConfirmed = true,
+                FirstName = "Joe",
+                LastName = "Smith",
+                Birthday = new DateTime(1990, 1, 1),
+                PlatformCustomerId = rndPlatformId,
+                GamBalance = new decimal(4200.54),
+                GamBalanceUpdOnUTC = DateTime.Now
+            };
+
+            // Truncate tables
+            //http://stackoverflow.com/questions/24209220/ef6-code-first-drop-tables-not-entire-database-when-model-changes
+
+            var fUser = manager.FindByEmail(newUser.Email);
+            if (fUser == null) {
+                manager.Create(newUser, "1q2w3e");
+            } else {
+                manager.Update(newUser);
             }
+
+            var custId = manager.FindByEmail(newUser.Email).Id;
+            return custId;
         }
     }
 }
