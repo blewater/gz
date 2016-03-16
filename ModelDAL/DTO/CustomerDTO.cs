403bc6f761454089b27b2f0bc90b8a10ed114c6c
npal <nick.palladinos@gmail.com>
2016-03-16 17:09:17 +0200

    Remove Password

--- ModelDAL/DTO/CustomerDTO.cs
+++ ModelDAL/DTO/CustomerDTO.cs
@@ -12,7 +12,6 @@ namespace gzWeb.Models {
 
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public bool EmailConfirmed { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
