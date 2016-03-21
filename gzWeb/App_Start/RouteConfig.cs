0b88e5e4ea8e3cb495799e8058e2cf83f229edc4
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2016-03-22 00:15:46 +0200

    ADD: RouteDebugger, /Glimpse.axd handler, routing rule, web.debug/release transformation for dbconnection, glimpse removal in release

--- gzWeb/App_Start/RouteConfig.cs
+++ gzWeb/App_Start/RouteConfig.cs
@@ -7,8 +7,6 @@ namespace gzWeb
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
