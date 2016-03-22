0bb694db73878abb77aae6115ae2da72ca3c7f70
Manolis Marinos <manolis.marinos@outlook.com>
2016-03-22 18:56:03 +0200

    UPDATE Portfolio (ADD tree-like svg for plans, FIX plans margin for non-desktop versions, ADD tooltip functionality to donut chart)

--- gzWeb/Scripts/_app/directives/gzPortfolioChart.js
+++ gzWeb/Scripts/_app/directives/gzPortfolioChart.js
@@ -16,9 +16,10 @@
                 var plans = [
                     { text: 'Aggressive', percent: scope.gzAggressive, color: '#227B46' },
                     { text: 'Moderate', percent: scope.gzModerate, color: '#64BF89' },
-                    { text: 'Conservative', percent: scope.gzConservative, color: '#B4DCC4' },
+                    { text: 'Conservative', percent: scope.gzConservative, color: '#B4DCC4' }
                 ];
-                
+                var roi = { text: "Current ROI", percent: scope.gzAverage }
+
                 var root = d3.select(element[0]);
                 var rootWidth = root.node().getBoundingClientRect().width;
                 var rootHeight = root.node().getBoundingClientRect().height;
@@ -26,7 +27,7 @@
                 var externalPadding = 10;
                 var donutThickness = 50;
                 var outerRadius = minDimension - externalPadding;
-                var extension = 5;
+                var extension = 8;
                 var innerRadius = minDimension - externalPadding - donutThickness;
                 var fontSize = 18 * outerRadius / 100;
                 var duration = 400;
@@ -37,6 +38,11 @@
                         .attr("height", minDimension * 2);
 
                 var group = canvas.append("g").attr("transform", "translate(" + minDimension + "," + minDimension + ")");
+
+                var tooltip = d3.tip().attr('class', 'tooltip');
+
+                canvas.call(tooltip);
+
                 var arc = d3.svg.arc()
                             .innerRadius(outerRadius - extension)
                             .padRadius(outerRadius - extension)
@@ -46,60 +52,72 @@
                     .value(function (d) { return d.percent; })
                     .sort(function (d, i) { return i; });
 
+                function showTooltip(text) {
+                    tooltip.html('<span>' + text + '</span>').style("opacity", 1).show();
+                };
+                function hideTooltip() {
+                    tooltip.hide().style("opacity", 0);
+                };
+                function moveTooltip() {
+                    tooltip.style("left", function() { return (d3.event.pageX) + "px"; })
+                           .style("top", function() { return (d3.event.pageY - 30) + "px"; });
+                };
+
                 var arcs =
                     group.selectAll(".arc")
                         .data(pie(plans))
                     .enter().append("g")
                         .attr("class", "arc")
+                        .attr('pointer-events', 'none')
                     .each(function (d) { d.outerRadius = outerRadius - extension; })
-                    .attr("d", arc)
-                    .on("mouseover", function () {
-                        var el = d3.select(this);
-                        el.select("path")
-                            .transition()
-                            .delay(duration / 4)
-                            .duration(duration / 2)
-                            .ease('linear')
-                            .attrTween("d", function (d) {
-                                var i = d3.interpolate(d.outerRadius, outerRadius);
-                                return function (t) {
-                                    return arc.outerRadius(i(t))(d);
-                                };
-                            });
-                        el.select("text")
-                            .transition()
-                            .delay(duration / 4)
-                            .duration(duration / 2)
-                            .attr("font-size", fontSize*1.2 + "px");
-                            //.attr("transform", function (d) {
-                            //    return "translate(" + arc.centroid(d) + ")";
-                            //});
-                    })
-                    .on("mouseout", function () {
-                        var el = d3.select(this);
-                        el.select("path")
-                            .transition()
-                            .delay(duration / 4)
-                            .duration(duration / 2)
-                            .ease('linear')
-                            .attrTween("d", function (d) {
-                                var i = d3.interpolate(outerRadius, outerRadius - extension);
-                                return function (t) {
-                                    return arc.outerRadius(i(t))(d);
-                                };
-                            });
-                        el.select("text")
-                            .transition()
-                            .delay(duration / 4)
-                            .duration(duration / 2)
-                            .attr("font-size", fontSize + "px");
-                            //.style('opacity', 0)
-                            //.attr("transform", function (d) {
-                            //    var c = arc.centroid(d);
-                            //    return "translate(" + 0 + "," + 0 + ")";
-                            //});
-                    });
+                        .attr("d", arc)
+                        .on("mouseover", function (d) {
+                            var el = d3.select(this);
+                            el.select("path")
+                                .transition()
+                                .delay(duration / 4)
+                                .duration(duration / 2)
+                                .ease('linear')
+                                .attrTween("d", function (d) {
+                                    var i = d3.interpolate(d.outerRadius, outerRadius);
+                                    return function (t) {
+                                        return arc.outerRadius(i(t))(d);
+                                    };
+                                });
+                            el.select("text")
+                                .transition()
+                                .delay(duration / 4)
+                                .duration(duration / 2)
+                                .attr("font-size", fontSize * 1.2 + "px");
+                            showTooltip(d.data.text);
+                        })
+                        .on("mouseout", function () {
+                            var el = d3.select(this);
+                            el.select("path")
+                                .transition()
+                                .delay(duration / 4)
+                                .duration(duration / 2)
+                                .ease('linear')
+                                .attrTween("d", function (d) {
+                                    var i = d3.interpolate(outerRadius, outerRadius - extension);
+                                    return function (t) {
+                                        return arc.outerRadius(i(t))(d);
+                                    };
+                                });
+                            el.select("text")
+                                .transition()
+                                .delay(duration / 4)
+                                .duration(duration / 2)
+                                .attr("font-size", fontSize + "px");
+                            hideTooltip();
+                        })
+                        .on("mousemove", moveTooltip);
 
+                arcs.transition()
+                    .delay(duration * 3)
+                    .duration(0)
+                    .attr('pointer-events', '');
+                
                 arcs.append("path")
                     .attr("fill", function (d) { return d.data.color; })
                     .transition()
@@ -133,36 +151,40 @@
                     .attr("transform", function (d) {
                         var c = arc.centroid(d);
                         return "translate(" + c[0]*0.8 + "," + c[1]*0.8 + ")";
-                        //return "translate(" + c + ")";
                     })
                     .text(function (d) { return d.data.percent; });
 
                 group.append("text")
-                    //.on("mouseover", function () {
-                    //    d3.select(this)
-                    //        .transition()
-                    //        .delay(duration / 4)
-                    //        .duration(duration / 2)
-                    //        .attr("font-size", "42px");
-                    //})
-                    //.on("mouseout", function () {
-                    //    d3.select(this)
-                    //        .transition()
-                    //        .delay(duration / 4)
-                    //        .duration(duration / 2)
-                    //        .attr("font-size", "38px");
-                    //})
                     .style("text-anchor", "middle")
                     .attr("font-weight", "bold")
                     .style("font-size", "0px")
                     .attr("fill", "#27A95C")
+                    .on("mouseover", function () {
+                        showTooltip(roi.text);
+                        d3.select(this)
+                            .transition()
+                            .delay(duration / 4)
+                            .duration(duration / 2)
+                            .style("font-size", "45px")
+                            .attr("dy", ".38em");
+                    })
+                    .on("mouseout", function () {
+                        hideTooltip();
+                        d3.select(this)
+                            .transition()
+                            .delay(duration / 4)
+                            .duration(duration / 2)
+                            .style("font-size", "38px")
+                            .attr("dy", ".35em");
+                    })
+                    .on("mousemove", moveTooltip)
                     .transition()
                     .delay(duration * 3)
                     .duration(duration * 2)
                     .ease('elastic')
                     .style("font-size", "38px")
                     .attr("dy", ".35em")
-                    .text(scope.gzAverage)
+                    .text(roi.percent)
                 ;
             }
         };
