8a48d65fb6692b98b980a673ae0c7f428bf626de
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2016-03-29 21:52:59 +0300

    ADD: Months input param to Customer Investment Balance task, connectionStrings.config included in project but not checked-in CHANGE: Name refactoring

--- ModelDAL/Repos/Interfaces/IInvBalanceRepo.cs
+++ ModelDAL/Repos/Interfaces/IInvBalanceRepo.cs
@@ -5,7 +5,8 @@ namespace gzDAL.Repos.Interfaces
     public interface IInvBalanceRepo
     {
         Dictionary<int, CustFundShareRepo.PortfolioFundDTO> GetCalcMonthlyBalancesForCustomer(int custId, int year, int month, decimal cashToInvest, out decimal monthlyBalance, out decimal invGainLoss);
-        void SaveCustTrxsBalances(int custId);
+        void SaveCustomerTrxsBalances(int customerId);
+        void SaveCustomerTrxsBalances(int custId, uint[] monthsToProc);
         void SaveDBbyTrxMonthlyBalanceForCustomer(int custId, int year, int month, decimal cashToInvest);
     }
 }
