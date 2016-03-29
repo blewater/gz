442e7f1246766fccfd54289b36cb3262a4b7ae35
xdinos <xdinos@gmail.com>
2016-03-29 14:40:20 +0300

    ADD: First Web API controller

---
+++ gzWeb/Controllers/InvestmentsApiController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using gzDAL.Models;
+using gzWeb.Models;
+
+namespace gzWeb.Controllers
+{
+    public class InvestmentsApiController : ApiController
+    {
+        public InvestmentsApiController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetPortfolios()
+        {
+            return TryExecute(() => _dbContext.Portfolios.Where(x => x.IsActive)
+                                              .Select(x => new
+                                                           {
+                                                                   x.Id,
+                                                                   x.RiskTolerance,
+                                                                   Funds =
+                                                                   x.PortFunds.Select(f => new {f.Fund.HoldingName, f.Weight})
+                                                           })
+                                              .ToList());
+        }
+
+        protected Response<T> TryExecute<T>(Func<T> func) where T : class
+        {
+            try
+            {
+                return new Response<T>(Request, func());
+            }
+            catch (Exception exception)
+            {
+                return new Response<T>(Request, null) {Error = new ErrorViewModel {Message = exception.Message}};
+            }
+        }
+
+        private readonly ApplicationDbContext _dbContext;
+    }
+}
---
+++ gzWeb/Models/ResponseViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace gzWeb.Models
+{
+    public class Response<T> : IHttpActionResult where T : class
+    {
+        public ErrorViewModel Error { get; set; }
+        public T Result { get; private set; }
+
+        public Response(HttpRequestMessage request, T result)
+        {
+            _request = request;
+            Result = result;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_request.CreateResponse(HttpStatusCode.OK, Result));
+        }
+
+        private readonly HttpRequestMessage _request;
+    }
+
+    public class ErrorViewModel
+    {
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+}
