52a0fb9858f50e7d4acfd9f3154155e77d478aad
Manolis Marinos <manolis.marinos@outlook.com>
2016-04-01 18:24:23 +0300

    Add Register/Login Actions

---
+++ gzWeb/Areas/Mvc/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace gzWeb.Areas.Mvc.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
