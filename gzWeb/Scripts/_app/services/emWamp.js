743c35ec9380df1f40862ba6b4b5f50d2fcfbeef
xdinos <xdinos@gmail.com>
2016-04-05 14:03:07 +0300

    refactoring of registration angular controller and wamp service

---
+++ gzWeb/Scripts/_app/services/emWamp.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    APP.factory("emWamp", ['$wamp', function($wamp) {
+
+        $wamp.open();
+
+            var service = {
+                call: function(uri, parameters) {
+                    var callReturn = $wamp.call(uri, [], parameters);
+
+                    var originalFunc = callReturn.then;
+                    callReturn.then = function(successCallback, failureCallback) {
+                        function success(d) {
+                            if (typeof (successCallback) === 'function')
+                                successCallback(d && d.kwargs);
+                        }
+
+                        function error(e) {
+                            if (typeof (failureCallback) === 'function')
+                                failureCallback(e.kwargs);
+                        }
+
+                        return originalFunc.call(callReturn, success, error);
+                    };
+
+                    return callReturn;
+                }
+            };
+
+            return service;
+        }
+    ]);
+
+})();
