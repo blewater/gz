69a6651af0698cfb0cc2db1a097d437b1e30ba45
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2016-04-07 00:34:43 +0300

    ADD: Cash Balance for sold portfolio CHANGE: Move PortfolioDTO to DTO folder, Wrap retry connection to util FIX: shares values when selling

--- ModelDAL/Repos/Interfaces/IInvBalanceRepo.cs
+++ ModelDAL/Repos/Interfaces/IInvBalanceRepo.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
+using gzDAL.DTO;
 
 namespace gzDAL.Repos.Interfaces
 {
     public interface IInvBalanceRepo
     {
-        Dictionary<int, CustFundShareRepo.PortfolioFundDTO> GetCalcMonthlyBalancesForCustomer(int custId, int yearCurrent, int monthCurrent, decimal cashToInvest, out decimal monthlyBalance, out decimal invGainLoss);
+        Dictionary<int, PortfolioFundDTO> GetCalcMonthlyBalancesForCustomer(int custId, int yearCurrent, int monthCurrent, decimal cashToInvest, out decimal monthlyBalance, out decimal invGainLoss);
         void SaveDBCustomerMonthlyBalancesByTrx(int customerId, string[] monthsToProc);
         void SaveDBCustomerMonthBalanceByCashInv(int customerId, int year, int month, decimal cashToInvest);
     }
