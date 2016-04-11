adb6a3d722fe2b5b8653da904c0570b850e71372
xdinos <xdinos@gmail.com>
2016-04-11 19:02:28 +0300

    integrating user registrastion

--- gzWeb/Scripts/_app/controllers/auth/registerCtrl.js
+++ gzWeb/Scripts/_app/controllers/auth/registerCtrl.js
@@ -1,40 +1,141 @@
 ﻿(function () {
     'use strict';
     var ctrlId = 'registerCtrl';
-    APP.controller(ctrlId, ['$scope', 'emWamp', 'constants', ctrlFactory]);
-    function ctrlFactory($scope, emWamp, constants) {
+    APP.controller(ctrlId, ['$scope', '$state', 'emWamp', 'constants', ctrlFactory]);
+    function ctrlFactory($scope, $state, emWamp, constants) {
         $scope.model = {
             email: null,
+            username: null,
             password: null,
-            dateOfBirth: null
+            firstname: null,
+            lastname: null,
+            yearOfBirth: null,
+            monthOfBirth: 1,
+            dayOfBirth: 1,
+            title: 'Mr.',
+            postalCode: null,
+            city: null,
+            currency: null,
+            country: null,
+            phobePrefix: null,
+            phobeNumber: null
         };
 
-        $scope.usernameValid = {};
-        $scope.emailValid = {};
+        $scope.years = [];
+        $scope.months = [
+            { value: 1, display: '01' },
+            { value: 2, display: '02' },
+            { value: 3, display: '03' },
+            { value: 4, display: '04' },
+            { value: 5, display: '05' },
+            { value: 6, display: '06' },
+            { value: 7, display: '07' },
+            { value: 8, display: '08' },
+            { value: 9, display: '09' },
+            { value: 10, display: '10' },
+            { value: 11, display: '11' },
+            { value: 12, display: '12' }
+        ];
+        $scope.daysOfMonth = [];
+        $scope.phonePrefixes = [];
+        $scope.titles = [
+            'Mr.',
+            'Ms.',
+            'Mrs.',
+            'Miss'
+        ];
+
+        $scope.usernameValidation = {
+            isValidating: false,
+            isAvailable: true,
+            error: ''
+        };
+        $scope.emailValidation = {
+            isValidating: false,
+            isAvailable: true,
+            error: ''
+        };
+        $scope.passwordValidation = {
+            //isValidating: false,
+            isValid: true,
+            error: ''
+        };
+
         $scope.currentIpCountry = '';
         $scope.countries = [];
         $scope.currencies = [];
 
-        $scope.validateUsername = function(username) {
-            return emWamp.call('/user/account#validateUsername', { username: username })
-                .then(function(result) {
-                    $scope.usernameValid = result;
-                }, logError);
+        $scope.resetEmailValidation = function () {
+            $scope.emailValidation.isValidating = false;
+            $scope.emailValidation.isAvailable = true;
+            $scope.emailValidation.error = '';
+        };
+        $scope.validateEmail = function (email) {
+            if (!$scope.emailValidation.isValidating) {
+                $scope.emailValidation.isValidating = true;
+                return emWamp.call('/user/account#validateEmail', { email: email })
+                    .then(function (result) {
+                        $scope.emailValidation.isValidating = false;
+                        $scope.emailValidation.isAvailable = result.isAvailable;
+                        $scope.emailValidation.error = result.error;
+                    }, function (error) {
+                        $scope.emailValidation.isValidating = false;
+                        logError(error);
+                    });
+            }
         };
 
-        $scope.validateEmail = function(email) {
-            return emWamp.call('/user/account#validateEmail', { email: email })
-                .then(function(result) {
-                    $scope.emailValid = result;
-                }, logError);
+        $scope.resetUsernameValidation = function () {
+            $scope.usernameValidation.isValidating = false;
+            $scope.usernameValidation.isAvailable = true;
+            $scope.usernameValidation.error = '';
+        };
+        $scope.validateUsername = function (username) {
+            if (!$scope.usernameValidation.isValidating) {
+                return emWamp.call('/user/account#validateUsername', { username: username })
+                    .then(function (result) {
+                        $scope.usernameValidation.isValidating = false;
+                        $scope.usernameValidation.isAvailable = result.isAvailable;
+                        $scope.usernameValidation.error = result.error;
+                    }, function (error) {
+                        $scope.usernameValidation.isValidating = false;
+                        logError(error);
+                    });
+            }
         };
 
+        $scope.resetPasswordValidation = function() {
+            $scope.passwordValidation.isValid = true;
+            $scope.passwordValidation.error = '';
+        };
         $scope.validatePassword = function (password) {
             if (_passwordPolicyRegEx.test(password)) {
-                return new { isValid: true };
+                $scope.passwordValidation.isValid = true;
             } else {
-                return new { isValid: false, error: _passwordPolicyError };
+                $scope.passwordValidation.isValid = false;
+                $scope.passwordValidation.error = _passwordPolicyError;
             };
+        };
+
+        $scope.proccedToUserDetails = function () {
+
+            if ($scope.userForm.$valid) {
+                $state.go('form.details');
+            } else {
+                return false;
+            }
+
+            return true;
+        };
+
+        $scope.onCountryChanged = function () {
+            var phonePrefix = '';
+            angular.forEach($scope.countries, function(value) {
+                if (value.code == $scope.model.country)
+                    phonePrefix = value.phonePrefix;
+            });
+            $scope.phonePrefixes.splice(0, $scope.phonePrefixes.length);
+            $scope.phonePrefixes.push(phonePrefix);
         };
 
         $scope.register = function(email, password, birthDate, currency) {
@@ -92,6 +193,21 @@
             getPasswordPolicy();
             getCountries();
             getCurrencies();
+
+            var maxYear = 100;
+            var year = new Date().getFullYear() - 18;
+            $scope.yearOfBirth = year;
+            do {
+                $scope.years.push(year);
+                //$angular.array($scope.years, year);
+                year--;
+                maxYear--;
+            } while (maxYear > 0);
+
+            for (var m = 1; m < 32; m++) {
+                $scope.daysOfMonth.push(m);
+            };
+
         };
 
         init();
