1d7c2e4dfa392d54399fee3e144ad3b46d8d6b9b
xdinos <xdinos@gmail.com>
2016-04-14 09:28:31 +0300

    minor refactoring

--- gzWeb/Scripts/_app/services/emWamp.js
+++ gzWeb/Scripts/_app/services/emWamp.js
@@ -1,7 +1,7 @@
 ﻿(function() {
-    'use strict';
+    "use strict";
 
-    APP.factory("emWamp", ['$wamp', emWampFunction]);
+    APP.factory("emWamp", ["$wamp", emWampFunction]);
     function emWampFunction($wamp) {
 
         var _logError = function(error) {
@@ -33,12 +33,48 @@
 
             call: _call,
 
-            login: function() {
-                
+            /// <summary>
+            /// Login the end-user.
+            /// </summary>
+            /// <parameters>
+            /// For clasic login:
+            ///     {
+            ///         usernameOrEmail: 'xx@xx.com',
+            ///         password: 'iampwd',
+            ///         captchaPublicKey: "6LcJ7e4SAAAAAOaigpBV8fDtQlWIDrRPNFHjQRqn",
+            ///         captchaChallenge: "03AHJ_VutMqFDQyKxHChZ6vF4pi8Zu76IzvP5YCNdZMeOdjVYpY",
+            ///         captchaResponse: "120"
+            ///     }
+            /// For external login:
+            ///     {
+            ///         "referrerID": "0fbfcca4166149f6a26798d3a2f90a76"
+            ///     }
+            /// </parameters>
+            userLogin: function(parameters) {
+                return _call("/user#login", parameters);
             },
 
-            logout: function() {
-                _call('/user#logout').then(function(result) {}, _logError);
+            /// <summary>
+            /// Get current session information.
+            /// <summary>
+            /// <returns>
+            /// {
+            ///     "isAuthenticated": true,
+            ///     "firstname": "Bruce",
+            ///     "surname": "Wliam",
+            ///     "currency": "EUR",
+            ///     "userCountry": "AS",
+            ///     "ipCountry": "ES",
+            ///     "loginTime": "2014-01-01T00:00:00",
+            ///     "isEmailVerified": true
+            ///     }
+            /// </returns>
+            userGetSessionInfo: function() {
+                return _call("/user#getSessionInfo");
+            },
+
+            userLogout: function() {
+                _call("/user#logout").then(function(result) {}, _logError);
             },
         };
 
