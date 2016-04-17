ae12fa72906ca1ac600adfe426a131cbda92aa42
Manolis Marinos <manolis.marinos@outlook.com>
2016-04-18 00:38:37 +0300

    Update message service

---
+++ gzWeb/Scripts/_app/services/nsMessagesService/nsPromptCtrl.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+    var ctrlId = 'nsPromptCtrl';
+    APP.controller(ctrlId, ['$scope', ctrlFactory]);
+    function ctrlFactory($scope) {
+        $scope._dispatchEventKeys = function(event, obj) {
+            if (obj.key === 'ok')
+                $scope.nsOk(true);
+            else if (obj.key === 'cancel')
+                $scope.nsCancel("cancel");
+        };
+        var unregisterBtnClickEvent = $scope.$on('btnClicked', function (event, obj) {
+            if (angular.isDefined($scope.dispatchEventKeys))
+                $scope.dispatchEventKeys(event, obj);
+            else
+                $scope._dispatchEventKeys(event, obj);
+        });
+        $scope.$on("$destroy", function () {
+            unregisterBtnClickEvent();
+        });
+    }
+})();
