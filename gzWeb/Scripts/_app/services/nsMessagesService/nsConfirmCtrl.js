3e4bd783652fa88207259e7d9f0112b571bdada0
Manolis Marinos <manolis.marinos@outlook.com>
2016-04-18 00:38:37 +0300

    Update message service

---
+++ gzWeb/Scripts/_app/services/nsMessagesService/nsConfirmCtrl.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+    var ctrlId = 'nsConfirmCtrl';
+    APP.controller(ctrlId, ['$scope', '$controller', ctrlFactory]);
+    function ctrlFactory($scope, $controller) {
+        $controller('nsPromptCtrl', { $scope: $scope });
+        $scope.dispatchEventKeys = function (event, obj) {
+            if (obj.key === 'ok')
+                $scope.nsOk(true);
+            else if (obj.key === 'cancel')
+                $scope.nsCancel('cancel');
+        };
+    }
+})();
