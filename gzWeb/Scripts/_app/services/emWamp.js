482b5c26ddfe897c681ecc5ba0e29f22d3fd11e7
xdinos <xdinos@gmail.com>
2016-04-19 16:02:49 +0300

    ADD: first draft of depositCtrl, more work on emWamp/emBank services

--- gzWeb/Scripts/_app/services/emWamp.js
+++ gzWeb/Scripts/_app/services/emWamp.js
@@ -98,7 +98,6 @@
     APP.factory("emCasino", ["emWamp", emCasinoFunc]);
     function emCasinoFunc(emWamp) {
 
-
         var _service = {};
 
         _service.FIELDS = {
@@ -174,30 +173,50 @@
         /// To reduce the size of the returned data for the method, this parameter is used to indicate the game fields expected in JSON response. 
         /// Individual field value is defined as below. Passing this field with the sum of the expected fields' value.
         /// </parameter>
-        _service.getGames = function (filterByName, filterBySlug, filterByVendor, filterByCategory, filterByTag, filterByAttribute, expectedFields, pageIndex, pageSize) {
-            //angular.forEach(ex)
-            return emWamp.call("/casino#getGames",
-            {
-                filterByName: filterByName,
-                filterBySlug: filterBySlug,
-                filterByVendor: filterByVendor,
-                filterByCategory: filterByCategory,
-                filterByTag: filterByTag,
-                filterByAttribute: filterByAttribute,
-                expectedFields: expectedFields,
-                expectedFormat: "array",
-                pageIndex: pageIndex,
-                pageSize: pageSize,
-            });
+        //_service.getGames = function (filterByName, filterBySlug, filterByVendor, filterByCategory, filterByTag, filterByAttribute, expectedFields, pageIndex, pageSize) {
+        //    //angular.forEach(ex)
+        //    return emWamp.call("/casino#getGames",
+        //    {
+        //        filterByName: filterByName,
+        //        filterBySlug: filterBySlug,
+        //        filterByVendor: filterByVendor,
+        //        filterByCategory: filterByCategory,
+        //        filterByTag: filterByTag,
+        //        filterByAttribute: filterByAttribute,
+        //        expectedFields: expectedFields,
+        //        expectedFormat: "array",
+        //        pageIndex: pageIndex,
+        //        pageSize: pageSize,
+        //    });
+        //};
+
+        _service.getGameCategories = function () {
+            return emWamp.call("/casino#getGameCategories");
+        };
+
+        _service.getGames = function(parameters) {
+            return emWamp.call("/casino#getGames", parameters);
+        };
+
+        _service.getGameVendors = function () {
+            return emWamp.call("/casino#getGameVendors");
         };
 
         return _service;
     };
 
-    APP.factory("emBanking", ["emWamp", emBankingFunc]);
-    function emBankingFunc(emWamp) {
+    APP.factory("emBanking", ["$q", "emWamp", emBankingFunc]);
+    function emBankingFunc($q, emWamp) {
 
         var _service = {};
+
+        var _supportedPaymentMethodCode = {
+            VISA: "VISA",
+            Maestro: "Maestro",
+            MasterCard: "MasterCard"
+        };
+
+        _service.PaymentMethodCode = _supportedPaymentMethodCode;
 
         /// <summary>
         /// Query the gaming accounts of the current logged-in user.
@@ -227,7 +246,20 @@
         _service.getPaymentMethods = function (filterByCountry, currency) {
             return emWamp.call("/user/deposit#getPaymentMethods", { filterByCountry: filterByCountry, currency: currency });
         };
-        
+
+        _service.getSupportedPaymentMethods = function (filterByCountry, currency) {
+            var q = $q.defer();
+            emWamp.call("/user/deposit#getPaymentMethods", { filterByCountry: filterByCountry, currency: currency })
+                .then(function (result) {
+                    var paymentMethods = [];
+                    angular.forEach(_supportedPaymentMethodCode, function (key, value) {
+                        paymentMethods.push(result.paymentMethods[key]);
+                    });
+                    q.resolve(paymentMethods);
+                });
+            return q.promise;
+        };
+
         //
         // Query the payment method grouped by categories.
         //
@@ -257,6 +289,150 @@
             return emWamp.call("/user/deposit#getPaymentMethodCfg", { paymentMethodCode: paymentMethodCode });
         };
 
+        //
+        // Register pay card for the current logged-in user.
+        //
+        // Parameter
+        // {
+        //      "paymentMethodCode": "MasterCard",
+        //      "fields": {
+        //          // the following fields are different per each payment method
+        //          // To get the field requirement for specific payment method, use /user/deposit#getPaymentMethodCfg
+        //
+        //          "cardNumber": "5138495125550554",
+        //          "cardHolderName": "5138495125550554",
+        //          "cardExpiryDate": "03/2014",
+        //          "cardValidFrom": "02/2012",
+        //          "cardIssueNumber": "123",
+        //          ...
+        //      }
+        // }
+        //
+        // paymentMethodCode  [string, mandatory]
+        // The identifier of the payment method, can be gotten from getCategorizedPagmentMethods.
+        //
+        // fields  [JSON object]
+        // The field requirement is different per different payment method and user. 
+        // Within the response of the call getPaymentMethodCfg, the properties of fields.payCardID.registrationFields 
+        // contain the information of the fields to register the paycard for each payment method.
+        _service.registerPayCard = function(parameter) {
+            return emWamp.call("/user/deposit#registerPayCard", parameter);
+        };
+
+        _service.registerPayCardVISA = function(cardNumber, cardHolderName, cardExpiryDate) {
+            return emWamp.call("/user/deposit#registerPayCard",
+            {
+                paymentMethodCode: _supportedPaymentMethodCode.VISA,
+                fields: {
+                    cardNumber: cardNumber,
+                    cardHolderName: cardHolderName,
+                    cardExpiryDate: cardExpiryDate
+                }
+            });
+        };
+
+        _service.registerPayCardMaestro = function (cardNumber, cardHolderName, cardExpiryDate, cardValidFrom, cardIssueNumber) {
+            return emWamp.call("/user/deposit#registerPayCard",
+            {
+                paymentMethodCode: _supportedPaymentMethodCode.Maestro,
+                fields: {
+                    cardNumber: cardNumber,
+                    cardHolderName: cardHolderName,
+                    cardExpiryDate: cardExpiryDate,
+                    cardValidFrom: cardValidFrom,
+                    cardIssueNumber: cardIssueNumber
+                }
+            });
+        };
+
+        _service.registerPayCardMasterCard = function (cardNumber, cardHolderName, cardExpiryDate, cardValidFrom, cardIssueNumber) {
+            return emWamp.call("/user/deposit#registerPayCard",
+            {
+                paymentMethodCode: _supportedPaymentMethodCode.MasterCard,
+                fields: {
+                    cardNumber: cardNumber,
+                    cardHolderName: cardHolderName,
+                    cardExpiryDate: cardExpiryDate,
+                    cardValidFrom: cardValidFrom,
+                    cardIssueNumber: cardIssueNumber
+                }
+            });
+        };
+
+        //
+        // Prepare the deposit transaction for the current logged-in user.
+        //
+        // parameters
+        // {
+        //    "paymentMethodCode": "VISA",
+        // 
+        //    // the following fields are different per each payment method
+        //    // the field requirement for each payment method can be queried from /user/deposit#getPaymentMethodCfg method
+        //    "fields":
+        //    {
+        //        "gamingAccountID": 429088,
+        //        "currency": "CNY",
+        //        "amount": "100",
+        //        "payCardID": "330126",
+        //        "cardSecurityCode": "123",
+        //        "bonusCode": "",
+        //    ...
+        //        }
+        // }
+        //
+        // paymentMethodCode  [string, mandatory]
+        // The identifier of the payment method, can be gotten from getCategorizedPagmentMethods.
+        //
+        // fields  [JSON object]
+        // The field requirement is different per different payment method and user.
+        // Using getPaymentMethodCfg to query the required field of the specific payment method.
+        //
+        _service.prepare = function (parameters) {
+            return emWamp.call("/user/deposit#prepare", parameters);
+        };
+
+        _service.prepareVisa = function(gamingAccountId, currency, amount, payCardId, cardSecurityCode) {
+            return emWamp.call("/user/deposit#prepare",
+            {
+                paymentMethodCode: _supportedPaymentMethodCode.VISA,
+                fields: {
+                    gamingAccountID: gamingAccountId,
+                    currency: currency,
+                    amount: amount,
+                    payCardID: payCardId,
+                    cardSecurityCode: cardSecurityCode
+                }
+            });
+        };
+
+        _service.prepareMaestro = function(gamingAccountId, currency, amount, payCardId, cardSecurityCode) {
+            return emWamp.call("/user/deposit#prepare",
+            {
+                paymentMethodCode: _supportedPaymentMethodCode.Maestro,
+                fields: {
+                    gamingAccountID: gamingAccountId,
+                    currency: currency,
+                    amount: amount,
+                    payCardID: payCardId,
+                    cardSecurityCode: cardSecurityCode
+                }
+            });
+        };
+
+        _service.prepareMasterCard = function (gamingAccountId, currency, amount, payCardId, cardSecurityCode) {
+            return emWamp.call("/user/deposit#prepare",
+            {
+                paymentMethodCode: _supportedPaymentMethodCode.MasterCard,
+                fields: {
+                    gamingAccountID: gamingAccountId,
+                    currency: currency,
+                    amount: amount,
+                    payCardID: payCardId,
+                    cardSecurityCode: cardSecurityCode
+                }
+            });
+        };
+        
         return _service;
     };
 })();
