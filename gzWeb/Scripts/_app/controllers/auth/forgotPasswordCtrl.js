675d4a716ca3a9f3174fda2cef8575e8150d62cf
Manolis Marinos <manolis.marinos@outlook.com>
2016-04-20 11:24:15 +0300

    Implement forgot password modal

---
+++ gzWeb/Scripts/_app/controllers/auth/forgotPasswordCtrl.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict";
+    var ctrlId = "forgotPasswordCtrl";
+    APP.controller(ctrlId, ["$scope", "$http", "emWamp", "api", "localStorageService", ctrlFactory]);
+    function ctrlFactory($scope, $http, emWamp, api, localStorageService) {
+        $scope.model = {
+            email: null
+        };
+
+        $scope.backToLogin = function () {
+        };
+        $scope.sendInstructions = function () {
+            if ($scope.form.$valid) {
+            }
+        };
+    }
+})();
