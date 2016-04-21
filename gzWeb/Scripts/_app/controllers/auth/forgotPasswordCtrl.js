a9f5bfc3f3de4b8f01cdb1176d4c3de615480dc8
Manolis Marinos <manolis.marinos@outlook.com>
2016-04-22 02:00:00 +0300

    Implement transitions between login/forgotPassword modals

--- gzWeb/Scripts/_app/controllers/auth/forgotPasswordCtrl.js
+++ gzWeb/Scripts/_app/controllers/auth/forgotPasswordCtrl.js
@@ -8,6 +8,13 @@
         };
 
         $scope.backToLogin = function () {
+            $scope.nsBack({
+                nsType: 'modal',
+                nsSize: 'sm',
+                nsTemplate: '/partials/messages/login.html',
+                nsCtrl: 'loginCtrl',
+                nsStatic: true,
+            });
         };
         $scope.sendInstructions = function () {
             if ($scope.form.$valid) {
