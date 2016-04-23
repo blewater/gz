06a0565c53f7655870492011a12982192ff06244
xdinos <xdinos@gmail.com>
2016-04-23 13:40:44 +0300

    ADD: helper class for currency

---
+++ ModelDAL/CurrencyHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace gzDAL
+{
+    public static class CurrencyHelper
+    {
+        private static readonly Dictionary<string, string> SymbolsByCode;
+
+        public static string GetSymbol(string code) { return SymbolsByCode[code]; }
+
+        static CurrencyHelper()
+        {
+            SymbolsByCode = new Dictionary<string, string>();
+
+            var regions = CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+                          .Select(x => new RegionInfo(x.LCID));
+
+            foreach (var region in regions)
+                if (!SymbolsByCode.ContainsKey(region.ISOCurrencySymbol))
+                    SymbolsByCode.Add(region.ISOCurrencySymbol, region.CurrencySymbol);
+        }
+    }
+}
