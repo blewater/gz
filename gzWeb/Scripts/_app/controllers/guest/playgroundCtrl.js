2f9a402de47d2709ba56caf352cdd01a0979e988
Manolis Marinos <manolis.marinos@outlook.com>
2016-04-29 18:29:19 +0300

    Update registration steps titles

--- gzWeb/Scripts/_app/controllers/guest/playgroundCtrl.js
+++ gzWeb/Scripts/_app/controllers/guest/playgroundCtrl.js
@@ -48,6 +48,7 @@
                 }
             });
         };
+        $scope.registerDetails();
         $scope.login = function () {
             message.open({
                 nsType: 'modal',
