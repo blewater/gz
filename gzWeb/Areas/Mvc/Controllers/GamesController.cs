fe37950d87ffc4b4985c36e7632c229549e28d47
Manolis Marinos <manolis.marinos@outlook.com>
2016-05-10 03:32:50 +0300

    Remove extra games pages

--- gzWeb/Areas/Mvc/Controllers/GamesController.cs
+++ gzWeb/Areas/Mvc/Controllers/GamesController.cs
@@ -4,22 +4,7 @@ namespace gzWeb.Areas.Mvc.Controllers
 {
     public class GamesController : Controller
     {
-        public ActionResult Page1()
-        {
-            return View();
-        }
-
-        public ActionResult Page2()
-        {
-            return View();
-        }
-
-        public ActionResult Page3()
-        {
-            return View();
-        }
-
-        public ActionResult Page4()
+        public ActionResult Games()
         {
             return View();
         }
