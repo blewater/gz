1e12b1cd6c92070f7dcc5e4ac5cc1eaec32cda03
Mario Karagiorgas <salem8@gmail.com>
2016-05-12 10:03:25 +0300

    ADD: InvestmentsController back

---
+++ gzWeb/Areas/Mvc/Controllers/InvestmentsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using gzWeb.Areas.Mvc.Models;
+using gzDAL.Models;
+using gzWeb.Models;
+using gzWeb.Utilities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using gzDAL.DTO;
+using gzDAL.Conf;
+
+namespace gzWeb.Areas.Mvc.Controllers
+{
+    // [Authorize]
+    public class InvestmentsController : Controller
+    {
+        public ActionResult Summary() { return View(); }
+        public ActionResult Portfolio() { return View(); }
+        public ActionResult Performance() { return View(); } 
+        public ActionResult Activity() { return View(); } 
+        
+
+
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Investments
+        [Authorize]
+        public ActionResult Index()
+        {
+            db.Database.Log = new DebugTextWriter().Write;
+
+            var manager = Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var customer = manager.FindById(User.Identity.GetUserId<int>());
+
+            var bal = customer.InvBalance;
+
+            var customerDto = new CustomerDTO();
+            Mapper.Map<ApplicationUser, CustomerDTO>(customer, customerDto);
+            return View(customerDto);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
