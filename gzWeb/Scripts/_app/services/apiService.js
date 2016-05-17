23ff7c5f9409a799b26f1ad234e23dd973c23dbe
Manolis Marinos <manolis.marinos@outlook.com>
2016-05-17 14:47:53 +0300

    Remove logout, add revokeRegistration (keep only api calls to apiService)

--- gzWeb/Scripts/_app/services/apiService.js
+++ gzWeb/Scripts/_app/services/apiService.js
@@ -1,9 +1,9 @@
 ﻿(function () {
     'use strict';
 
-    APP.factory('api', ['$http', '$rootScope', 'localStorageService', 'constants', serviceFactory]);
+    APP.factory('api', ['$http', '$rootScope', serviceFactory]);
 
-    function serviceFactory($http, $rootScope, localStorageService, constants) {
+    function serviceFactory($http, $rootScope) {
         var factory = {};
 
         // #region Urls
@@ -17,23 +17,6 @@
             auth: apiBaseUrl('auth')
         };
         factory.urls = urls;
-        // #endregion
-
-        // #region xdinos refactoring
-        //function httpGet(url) {
-
-        //    var authData = localStorageService.get(constants.storageKeys.authData);
-        //    var authHeaders = {};
-        //    if (authData) {
-        //        authHeaders.Authorization = "Bearer " + authData.token;
-        //    };
-
-        //    return $http({
-        //        url: url,
-        //        method: "GET",
-        //        headers: authHeaders
-        //    });
-        //}
         // #endregion
 
         // #region Common
@@ -66,35 +49,6 @@
         };
         // #endregion
 
-        // #region Investments
-        factory.getSummaryData = function () {
-            return $http.get(urls.investments + 'getSummaryData');
-            //return httpGet(urls.investments + 'getSummaryData');
-        };
-        factory.transferCashToGames = function () {
-            return $http.post(urls.investments + 'transferCashToGames');
-        }
-
-        factory.getPortfolioData = function () {
-            return $http.get(urls.investments + 'getPortfolioData');
-            //return httpGet(urls.investments + 'getPortfolioData');
-        };
-        factory.setPlanSelection = function () {
-            return $http.post(urls.investments + 'setPlanSelection');
-        }
-
-        factory.getPerformanceData = function () {
-            return $http.get(urls.investments + 'getPerformanceData');
-            //return httpGet(urls.investments + 'getPerformanceData');
-        };
-        // #endregion
-
-        // #region Games
-        // #endregion
-
-        // #region Guest
-        // #endregion
-
         // #region Auth
         factory.login = function (usernameOrEmail, password) {
             var data = "grant_type=password" +
@@ -108,39 +62,49 @@
             });
         }
 
-        factory.logout = function () {
-            localStorageService.remove(constants.storageKeys.authData);
-            // TODO
-            //var templates = $filter('toArray')(constants.templates);
-            //for (var i = 0; i < templates.length; i++)
-            //    $templateCache.remove(templates[i]);
-            //message.clear();
-        }
-
         factory.register = function (parameters) {
             return $http.post('/api/Account/Register', parameters);
         }
 
+        factory.revokeRegistration = function () {
+            return $http.post('/api/Account/RevokeRegistration');
+        }
+
         factory.forgotPassword = function (email) {
-            return $http({
-                url: 'api/Account/ForgotPassword',
-                method: 'POST',
-                data: { Email: email }
-            });
+            return $http.post('/api/Account/ForgotPassword', { Email: email });
         }
 
         factory.resetPassword = function (parameters) {
-            return $http({
-                url: 'api/Account/ResetPassword',
-                method: 'POST',
-                data: {
-                    Email: email,
-                    Password: $scope.model.password,
-                    ConfirmPassword: $scope.model.password,
-                    Code: $scope.model.code
-                }
-            });
+            return $http.post('/api/Account/ResetPassword', parameters);
         }
+
+        factory.changePassword = function (parameters) {
+            return $http.post('/api/Account/ChangePassword', parameters);
+        }
+        // #endregion
+
+        // #region Investments
+        factory.getSummaryData = function () {
+            return $http.get(urls.investments + 'getSummaryData');
+        };
+        factory.transferCashToGames = function () {
+            return $http.post(urls.investments + 'transferCashToGames');
+        }
+        factory.getPortfolioData = function () {
+            return $http.get(urls.investments + 'getPortfolioData');
+        };
+        factory.setPlanSelection = function () {
+            return $http.post(urls.investments + 'setPlanSelection');
+        }
+        factory.getPerformanceData = function () {
+            return $http.get(urls.investments + 'getPerformanceData');
+        };
+        // #endregion
+
+        // #region Games
+        // #endregion
+
+        // #region Guest
         // #endregion
 
         return factory;
