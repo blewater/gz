9df29fcc1b88cb5b2e752d3555d3ed1800d52f58
Mario Karagiorgas <salem8@gmail.com>
2016-05-23 02:30:29 +0300

    CHANGE: Split GzTransactions to GmTrx, Type, GzTrx, Type
    Add unique index on GMCustomerId, TotalDeposits made a separate method.
    ----------------------------------------------
    UPDATE gztrxs
    SET TypeId = TYPEID -1
    WHERE TYPEID IN (2,3)
    GO
    UPDATE gztrxs
    SET TypeId = 3
    WHERE TYPEID = 5
    GO
    UPDATE gztrxs
    SET TypeId = TYPEID -3
    WHERE TYPEID IN (7,8,9,10)
    GO
    INSERT INTO GmTrxs
    (CustomerId, YearMonthCtd, TypeId, Amount, CreatedOnUtc)
    SELECT CustomerId, YearMonthCtd, TypeId, Amount, CreatedOnUtc
    from GzTrxs
    where typeid = 1 
    go
    delete gztrxs where TypeId = 1
    go
    INSERT INTO GmTrxs
    (CustomerId, YearMonthCtd, TypeId, Amount, CreatedOnUtc)
    SELECT CustomerId, YearMonthCtd, 2, Amount, CreatedOnUtc
    from GzTrxs
    where typeid = 4 
    go
    delete gztrxs where TypeId = 4
    go
    INSERT INTO GmTrxs
    (CustomerId, YearMonthCtd, TypeId, Amount, CreatedOnUtc)
    SELECT CustomerId, YearMonthCtd, 3, Amount, CreatedOnUtc
    from GzTrxs
    where typeid = 6 
    go
    delete gztrxs where TypeId = 6
    go
    delete GzTrxTypes where Id > 7
    go

---
+++ ModelDAL/Models/GmTrx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gzDAL.Models {
+
+    /// <summary>
+    /// 
+    /// Transfers between Casino <--> Greenzorro accounts, Casino <--> Customer bank account
+    /// 
+    /// </summary>
+    [Table("GmTrxs")]
+    public class GmTrx {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, Index("CustomerId_Mon_idx_gztransaction", IsUnique = false, Order = 1)]
+        public int CustomerId { get; set; }
+        [ForeignKey("CustomerId")]
+        public virtual ApplicationUser Customer { get; set; }
+
+        [Index("CustomerId_Mon_idx_gztransaction",IsUnique = false, Order = 2)]
+        [Required, StringLength(6)]
+        public string YearMonthCtd { get; set; }
+
+        [ForeignKey("Type")]
+        public int TypeId { get; set; }
+        public virtual GmTrxType Type { get; set; }
+
+        [Required]
+        public DateTime CreatedOnUtc { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
