0ff48472175d6e01b046b7efff5549a3d6374623
Manolis Marinos <manolis.marinos@outlook.com>
2016-05-30 11:50:47 +0300

    Fix login redirect issue

--- gzWeb/Scripts/_app/app.js
+++ gzWeb/Scripts/_app/app.js
@@ -42,51 +42,51 @@ var APP = (function () {
 
             auth.init();
 
-            $rootScope.$on(constants.events.ON_INIT, function () {
-                var currentRoute = $filter('where')(constants.routes.all, { 'path': $location.path() })[0];
-                if (!auth.authorize(currentRoute.roles))
+            var currentRoute = $filter('where')(constants.routes.all, { 'path': $location.path() })[0];
+            if (!auth.authorize(currentRoute.roles))
+                $location.path(constants.routes.home.path);
+
+            $rootScope.$on('$routeChangeStart', function (event, next, current) {
+                $rootScope.loading = true;
+                if (next && !auth.authorize(next.roles))
                     $location.path(constants.routes.home.path);
+            });
 
-                $rootScope.$on('$routeChangeStart', function (event, next, current) {
-                    $rootScope.loading = true;
-                    if (next && !auth.authorize(next.roles))
-                        $location.path(constants.routes.home.path);
-                });
+            $rootScope.$on('$routeChangeSuccess', function () {
+                $rootScope.loading = false;
+                $rootScope.documentTitle = constants.title;
+                if ($route.current.$$route && $route.current.$$route.title) {
+                    $rootScope.title = $route.current.$$route.title;
+                    $rootScope.documentTitle += " - " + $rootScope.title;
+                }
+            });
 
-                $rootScope.$on('$routeChangeSuccess', function () {
-                    $rootScope.loading = false;
-                    $rootScope.documentTitle = constants.title;
-                    if ($route.current.$$route && $route.current.$$route.title) {
-                        $rootScope.title = $route.current.$$route.title;
-                        $rootScope.documentTitle += " - " + $rootScope.title;
-                    }
-                });
-
-                $rootScope.xs = screenSize.on('xs', function (match) { $rootScope.xs = match; });
-                $rootScope.sm = screenSize.on('sm', function (match) { $rootScope.sm = match; });
-                $rootScope.md = screenSize.on('md', function (match) { $rootScope.md = match; });
-                $rootScope.lg = screenSize.on('lg', function (match) { $rootScope.lg = match; });
+            $rootScope.xs = screenSize.on('xs', function (match) { $rootScope.xs = match; });
+            $rootScope.sm = screenSize.on('sm', function (match) { $rootScope.sm = match; });
+            $rootScope.md = screenSize.on('md', function (match) { $rootScope.md = match; });
+            $rootScope.lg = screenSize.on('lg', function (match) { $rootScope.lg = match; });
+            $rootScope.size = screenSize.get();
+            screenSize.on('xs,sm,md,lg', function () {
                 $rootScope.size = screenSize.get();
-                screenSize.on('xs,sm,md,lg', function () {
-                    $rootScope.size = screenSize.get();
-                });
+            });
 
-                $rootScope.scrolled = false;
-                $rootScope.scrollOffset = 0;
-                angular.element($window).bind("scroll", function () {
-                    $rootScope.scrolled = this.pageYOffset > 0;
-                    $rootScope.scrollOffset = this.pageYOffset;
-                    $rootScope.$apply();
-                });
+            $rootScope.scrolled = false;
+            $rootScope.scrollOffset = 0;
+            angular.element($window).bind("scroll", function () {
+                $rootScope.scrolled = this.pageYOffset > 0;
+                $rootScope.scrollOffset = this.pageYOffset;
+                $rootScope.$apply();
+            });
 
+            $timeout(function () {
+                var $preloader = angular.element(document.querySelector('#preloader'));
+                $preloader.addClass('die');
+                $timeout(function () { $preloader.remove(); }, 2000);
+            }, 1000);
+
+            $rootScope.$on(constants.events.ON_INIT, function () {
                 $rootScope.loading = false;
                 $rootScope.initialized = true;
-
-                $timeout(function () {
-                    var $preloader = angular.element(document.querySelector('#preloader'));
-                    $preloader.addClass('die');
-                    $timeout(function () { $preloader.remove(); }, 2000);
-                }, 1000);
             });
         }
     ]);
