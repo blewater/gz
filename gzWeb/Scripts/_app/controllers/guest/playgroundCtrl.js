d3f9cdc2e3c2ea804a0f62448c5d631cbef12f2c
Manolis Marinos <manolis.marinos@outlook.com>
2016-05-31 14:20:16 +0300

    Update playground

--- gzWeb/Scripts/_app/controllers/guest/playgroundCtrl.js
+++ gzWeb/Scripts/_app/controllers/guest/playgroundCtrl.js
@@ -3,7 +3,6 @@
     var ctrlId = 'playgroundCtrl';
     APP.controller(ctrlId, ['$scope', 'message', 'emWamp', 'chat', '$location', 'auth', ctrlFactory]);
     function ctrlFactory($scope, message, emWamp, chat, $location, auth) {
-        // #region Playground
         // #region Messages
         var m = 1, n = 1, t = 1;
         $scope.alert = function () {
@@ -133,6 +132,18 @@
             });
         }
         // #endregion
+
+        // #region Account Management
+        $scope.transactionHistory = function () {
+            message.open({
+                nsType: 'modal',
+                nsSize: '600px',
+                nsTemplate: '/partials/messages/registerAccount.html',
+                nsCtrl: 'registerAccountCtrl',
+                nsStatic: true,
+            });
+        };
+        $scope.transactionHistory();
         // #endregion
     }
 })();
