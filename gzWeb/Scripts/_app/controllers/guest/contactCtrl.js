546b13be4e838a9aa47faea36cdf86bcb1806429
Giorgos Palaiologos <geo.pal@hotmail.com>
2016-06-08 20:14:53 +0300

    Add Contact page (alpha)

--- gzWeb/Scripts/_app/controllers/guest/contactCtrl.js
+++ gzWeb/Scripts/_app/controllers/guest/contactCtrl.js
@@ -4,4 +4,24 @@
     APP.controller(ctrlId, ['$scope', ctrlFactory]);
     function ctrlFactory($scope) {
     }
+
+    function initialize() {
+        var myLatLng = { lat: 51.5196508, lng: -0.1007646 };
+
+        var mapProp = new google.maps.Map(document.getElementById('gz-map'), {
+            center: myLatLng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: mapProp,
+            title: 'Greenzorro LTD. 80-83 , Long Lane LONDON EC1A 9ET'
+        });
+
+    };
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
 })();
