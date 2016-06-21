f6a58ec109c628d4cd5e889f74bd5e491ef03158
Giorgos Palaiologos <geo.pal@hotmail.com>
2016-06-21 19:46:16 +0300

    Update admin/logs page

--- gzWeb/Areas/Admin/Models/LogViewModel.cs
+++ gzWeb/Areas/Admin/Models/LogViewModel.cs
@@ -7,7 +7,10 @@ using System.Web;
 namespace gzWeb.Areas.Admin.Models
 {
     public class LogViewModel
-    {
+    {        
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string LogLevel { get; set; }
         public DateTime FromDate { get; set; }
         public DateTime ToDate { get; set; }
         public List<LogEntry> LogEntries { get; set; }
