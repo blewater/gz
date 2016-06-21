a2c5b54294de228a86e6e35a9c6d74d3280c29ce
Giorgos Palaiologos <geo.pal@hotmail.com>
2016-06-21 11:41:31 +0300

    Add LogsController

---
+++ gzWeb/Areas/Admin/Controllers/LogsController.cs
@@ -0,0 +1,20 @@
+﻿using gzDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gzWeb.Areas.Admin.Controllers
+{
+    public class LogsController : Controller
+    {
+        // GET: Admin/Logs
+        public ActionResult Index()
+        {
+            List<LogEntry> model = null;
+
+            return View("Index", model);
+        }
+    }
+}
