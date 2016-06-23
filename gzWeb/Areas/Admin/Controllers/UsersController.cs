95baf1cedfdd6b4a854d2b179fb7244cfc259c3c
xdinos <xdinos@gmail.com>
2016-06-23 11:18:29 +0300

    ADD: users admin page

---
+++ gzWeb/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using gzDAL.Models;
+
+namespace gzWeb.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public UsersController()
+        {
+            // TODO: (xdinos) inject
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            return View(_dbContext.Users.ToList());
+        }
+    }
+}
