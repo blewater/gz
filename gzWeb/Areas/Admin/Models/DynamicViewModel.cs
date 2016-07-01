85e72eb7673e875b590dddf647b0d8037a3b28b8
Giorgos Palaiologos <geo.pal@hotmail.com>
2016-07-01 15:49:17 +0300

    ADD: admin/DynamicPages

---
+++ gzWeb/Areas/Admin/Models/DynamicViewModel.cs
@@ -0,0 +1,16 @@
+﻿using gzDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gzWeb.Areas.Admin.Models
+{
+    public class DynamicViewModel
+    {        
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string SearchTerm { get; set; }
+        public List<DynamicPage> DynamicEntries { get; set; }
+    }
+}
