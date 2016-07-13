4ce321d72092fad402343aac530c033dd33f0ce8
Mario Karagiorgas <salem8@gmail.com>
2016-07-13 11:12:29 +0300

    CHANGE: SQL Project: Sync local db

--- sql/LocalDb/dbo/Functions/GetMonthsTrxAmount.sql
+++ sql/LocalDb/dbo/Functions/GetMonthsTrxAmount.sql
@@ -1,16 +1,15 @@
 ﻿CREATE FUNCTION [dbo].[GetMonthsTrxAmount]
-(
-	@CustomerId INT,
-	@YearMonth NVARCHAR(6),
-	@TypeCodeId INT
-)
-RETURNS TABLE AS RETURN
-(
-	SELECT T.Amount FROM GzTrxs T 
-		JOIN GzTrxTypes TT
-			ON T.TypeId = TT.Id
-		WHERE CustomerId = @CustomerId 
-			AND YearMonthCtd = @YearMonth
-			AND TT.Code = @TypeCodeId
-
-)
+                (
+	                @CustomerId INT,
+	                @YearMonth CHAR(6),
+	                @TypeCodeId INT
+                )
+                RETURNS TABLE AS RETURN
+                (
+	                SELECT T.Amount FROM GzTrxs T 
+		                JOIN GzTrxTypes TT
+			                ON T.TypeId = TT.Id
+		                WHERE CustomerId = @CustomerId 
+			                AND YearMonthCtd = @YearMonth
+			                AND TT.Code = @TypeCodeId
+                );
---
+++ sql/LocalDb/dbo/Tables/CarouselEntries.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[CarouselEntries] (
+    [Id]               INT            IDENTITY (1, 1) NOT NULL,
+    [Code]             NVARCHAR (100) NOT NULL,
+    [Title]            NVARCHAR (MAX) NOT NULL,
+    [SubTitle]         NVARCHAR (MAX) NOT NULL,
+    [ActionUrl]        NVARCHAR (MAX) NOT NULL,
+    [BackroudImageUrl] NVARCHAR (MAX) NOT NULL,
+    [Live]             BIT            NOT NULL,
+    [LiveFrom]         DATETIME       NOT NULL,
+    [LiveTo]           DATETIME       NOT NULL,
+    CONSTRAINT [PK_dbo.CarouselEntries] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [CarouselEntry_Code]
+    ON [dbo].[CarouselEntries]([Code] ASC);
+
