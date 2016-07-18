1965509a882a9b6e80d0299e5a7a49266b377346
Mario Karagiorgas <salem8@gmail.com>
2016-07-18 17:16:01 +0300

    ADD: Cpc logging, start new injest GM trx task

--- CustPortfolioCalcLib/Task/ProcessGmTrxTask.cs
+++ CustPortfolioCalcLib/Task/ProcessGmTrxTask.cs
@@ -2,22 +2,21 @@
 using gzDAL.Repos;
 
 namespace gzCpcLib.Task {
-
     /// <summary>
     /// 
     /// Request latest stock market pricing info for the interested Funds
     /// 
     /// </summary>
-    public class FundsUpdTask : CpcTask {
+    public class ProcessGmTrxTask : CpcTask {
 
         private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
 
         public override void DoTask() {
 
-            _logger.Info("Entered FundsUpdTask.DoTask()");
+            _logger.Info("Entered ProcessGmTrxTask.DoTask()");
 
-            var fundRepo = new FundRepo(new ApplicationDbContext());
-            fundRepo.SaveDbDailyFundClosingPrices();
+            //var fundRepo = new FundRepo(new ApplicationDbContext());
+            //fundRepo.SaveDbDailyFundClosingPrices();
         }
     }
 }
