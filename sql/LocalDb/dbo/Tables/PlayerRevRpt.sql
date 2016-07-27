05941fd97edad6ebac56ee9a4f02e94b8c83b7c6
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2016-07-27 17:42:02 +0300

    SQL Project: Changes local <--> dev

--- sql/LocalDb/dbo/Tables/PlayerRevRpt.sql
+++ sql/LocalDb/dbo/Tables/PlayerRevRpt.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[PlayerRevRpt] (
-    [RowNum]                INT           NOT NULL,
-    [User ID]               BIGINT        NOT NULL,
+    [ID]                    INT           IDENTITY (1, 1) NOT NULL,
+    [User ID]               INT           NOT NULL,
     [Username]              VARCHAR (256) NOT NULL,
     [YearMonth]             CHAR (6)      NOT NULL,
     [YearMonthDay]          CHAR (8)      NOT NULL,
@@ -8,15 +8,20 @@
     [Player status]         VARCHAR (256) NOT NULL,
     [Block reason]          VARCHAR (256) NULL,
     [Email address]         VARCHAR (256) NOT NULL,
+    [Last login]            DATETIME      NULL,
     [Total deposits amount] MONEY         NOT NULL,
     [Currency]              CHAR (3)      NOT NULL,
     [Net revenue]           MONEY         NOT NULL,
     [Gross revenue]         MONEY         NOT NULL,
     [NetLossInUSD]          MONEY         NOT NULL,
     [Real money balance]    MONEY         NOT NULL,
+    [Processed]             BIT           NOT NULL,
     [CreatedOnUtc]          DATETIME      NOT NULL,
-    [UpdatedOnUtc]          DATETIME      NOT NULL
+    [UpdatedOnUtc]          DATETIME      NOT NULL,
+    CONSTRAINT [PK_PlayerRevRpt_ID] PRIMARY KEY NONCLUSTERED ([ID] ASC)
 );
+
+
 
 
 GO
