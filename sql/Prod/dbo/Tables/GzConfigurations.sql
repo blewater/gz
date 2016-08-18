fd88d7405723175061533d4df27abf99131319b5
Mario Karagiorgas <salem8@gmail.com>
2016-08-18 10:46:33 +0300

    ADD: SQL gzconfiguration

--- sql/Prod/dbo/Tables/GzConfigurations.sql
+++ sql/Prod/dbo/Tables/GzConfigurations.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[GzConfigurations] (
-    [Id]               INT  IDENTITY (1, 1) NOT NULL,
-    [LOCK_IN_NUM_DAYS] INT  NOT NULL,
-    [COMMISSION_PCNT]  REAL NOT NULL,
-    [FUND_FEE_PCNT]    REAL NOT NULL,
-    [CREDIT_LOSS_PCNT] REAL NOT NULL,
+    [Id]                       INT  IDENTITY (1, 1) NOT NULL,
+    [LOCK_IN_NUM_DAYS]         INT  NOT NULL,
+    [COMMISSION_PCNT]          REAL NOT NULL,
+    [FUND_FEE_PCNT]            REAL NOT NULL,
+    [CREDIT_LOSS_PCNT]         REAL NOT NULL,
+    [FIRST_PORTFOLIO_RISK_VAL] INT  DEFAULT ((3)) NOT NULL,
     CONSTRAINT [PK_dbo.GzConfigurations] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
