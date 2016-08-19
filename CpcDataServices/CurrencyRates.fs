4ceb980948fd138ae4e29e1f1a66d433ec31dad1
Mario Karagiorgas <salem8@gmail.com>
2016-08-19 21:44:46 +0300

    CHANGE: GmTrx Refactoring

---
+++ CpcDataServices/CurrencyRates.fs
@@ -0,0 +1,36 @@
+﻿namespace CpcDataServices
+
+open FSharp.Data
+open System.Collections.Generic
+
+module public CurrencyRates = 
+    type UsdRates = JsonProvider<"../CpcDataServices/openexchangerates.json">
+
+    /// <summary>
+    ///
+    /// httpGet currency rates based on the UsdRates type
+    /// 
+    /// </summary>
+    /// <param name="currencyApiUrl"></param>
+    /// <return>Map of rates</return>
+    let getCurrencyRates (currencyApiUrl : string) : Map<string, decimal> = 
+        let usdRateTo = UsdRates.Load(currencyApiUrl)
+        let usdRates = [
+                        "USDAUD", usdRateTo.Rates.Aud ;
+                        "USDCHF", usdRateTo.Rates.Chf ;
+                        "USDDKK", usdRateTo.Rates.Dkk ;
+                        "USDEUR", usdRateTo.Rates.Eur ;
+                        "USDGBP", usdRateTo.Rates.Gbp ;
+                        "USDNOK", usdRateTo.Rates.Nok ;
+                        "USDSEK", usdRateTo.Rates.Sek ;
+                        "AUDUSD", 1m/usdRateTo.Rates.Aud ;
+                        "CHFUSD", 1m/usdRateTo.Rates.Chf ;
+                        "DKKUSD", 1m/usdRateTo.Rates.Dkk ;
+                        "EURUSD", 1m/usdRateTo.Rates.Eur ;
+                        "GBPUSD", 1m/usdRateTo.Rates.Gbp ;
+                        "NOKUSD", 1m/usdRateTo.Rates.Nok ;
+                        "SEKUSD", 1m/usdRateTo.Rates.Sek] |> Map.ofList
+        usdRates
+    
+    let setDbRates (db : DbUtil.DbSchema.ServiceTypes.SimpleDataContextTypes.GzDevDb) (usdRates : Map<string, decimal>) = 
+        printfn "incomplete..."
---
+++ CpcDataServices/DbUtil.fs
@@ -0,0 +1,26 @@
+﻿namespace CpcDataServices
+
+open FSharp.Data.TypeProviders
+
+module DbUtil =
+
+    // Use for compile time memory schema representation
+    [<Literal>]
+    let CompileTimeDbString = 
+        @"Data Source=.\SQLEXPRESS;Initial Catalog=gzDevDb;Persist Security Info=True;Integrated Security=SSPI;"
+    
+    type DbSchema = SqlDataConnection< ConnectionString=CompileTimeDbString >
+
+    /// <summary>
+    ///
+    /// Get a database object by creating a new DataContext and opening a database connection
+    ///
+    /// </summary>
+    /// <param name="dbConnectionString">The database connection string to use</param>
+    /// <returns>A newly created database object</returns>
+    let getOpenDb (dbConnectionString : string) : DbSchema.ServiceTypes.SimpleDataContextTypes.GzDevDb= 
+        // Open db
+        let db = DbSchema.GetDataContext(dbConnectionString)
+        //db.DataContext.Log <- System.Console.Out
+        db.Connection.Open()
+        db
