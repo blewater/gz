6f92789325cd98fbb29f5b82ab5da94f58a0ed64
xdinos <xdinos@gmail.com>
2016-08-23 16:18:21 +0300

    ADD: gzWeb.Admin

---
+++ gzWeb.Admin/Models/EmailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using gzDAL.Models;
+
+namespace gzWeb.Admin.Models
+{
+    public class EmailsViewModel
+    {        
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string SearchTerm { get; set; }
+        public List<EmailTemplate> EmailsEntries { get; set; }
+    }
+}
