b220f9568ffb84a0a507767ab4ccc5febae12b20
Manolis Marinos <manolis.marinos@outlook.com>
2016-09-13 17:59:09 +0300

    Update footer with license disclaimer

--- gzWeb/Scripts/_app/controllers/templates/footerCtrl.js
+++ gzWeb/Scripts/_app/controllers/templates/footerCtrl.js
@@ -1,12 +1,13 @@
 ﻿(function () {
     'use strict';
     var ctrlId = 'footerCtrl';
-    APP.controller(ctrlId, ['$scope', ctrlFactory]);
-    function ctrlFactory($scope) {
+    APP.controller(ctrlId, ['$scope', 'localStorageService', 'constants', ctrlFactory]);
+    function ctrlFactory($scope, localStorageService, constants) {
         $scope.copyrightYears = (function () {
             var startingYear = 2016;
             var currentYear = moment().year();
             return currentYear === startingYear ? startingYear : startingYear + " - " + currentYear;
         })();
+        $scope.inDebugMode = localStorageService.get(constants.storageKeys.debug);
     }
 })();
