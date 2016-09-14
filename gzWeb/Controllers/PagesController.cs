ad2e65143598183c40ab5326330b791f87e73785
xdinos <xdinos@gmail.com>
2016-09-14 21:53:25 +0300

    PageController fixes

--- gzWeb/Controllers/PagesController.cs
+++ gzWeb/Controllers/PagesController.cs
@@ -6,7 +6,6 @@ using gzDAL.Models;
 
 namespace gzWeb.Controllers
 {
-    [Authorize]
     [RoutePrefix("api/Pages")]
     public class PagesController : BaseApiController
     {
@@ -17,13 +16,15 @@ namespace gzWeb.Controllers
         }
 
         [Route("Carousel")]
+        [HttpGet]
         public IHttpActionResult Carousel()
         {
             var now = DateTime.UtcNow;
-            return OkMsg(() => _dbContext.CarouselEntries.Where(x => x.Live && x.LiveFrom >= now && x.LiveTo <= now));
+            return OkMsg(() => _dbContext.CarouselEntries.Where(x => x.Live && now>=x.LiveFrom && now<=x.LiveTo));
         }
 
         [Route("Page")]
+        [HttpGet]
         public IHttpActionResult Page(string code)
         {
             return OkMsg(() => _dbContext.DynamicPages.SingleOrDefault(x => x.Code == code));
