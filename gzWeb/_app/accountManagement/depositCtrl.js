7b4db781ce351caab101bc69dff87d85cda8370d
Manolis Marinos <manolis.marinos@outlook.com>
2016-09-15 16:44:05 +0300

    Disable deposit/withdraw button while processing, Change selected method display name

--- gzWeb/_app/accountManagement/depositCtrl.js
+++ gzWeb/_app/accountManagement/depositCtrl.js
@@ -162,5 +162,18 @@
         };
         // #endregion
 
+        $scope.getMethodName = function (method) {
+            switch (method.code) {
+                case emBanking.PaymentMethodCode.VISA:
+                case emBanking.PaymentMethodCode.Maestro:
+                case emBanking.PaymentMethodCode.MasterCard:
+                case emBanking.PaymentMethodCode.MoneyMatrixCreditCard:
+                    return method.name;
+                case emBanking.PaymentMethodCode.MoneyMatrixTrustly:
+                    return "Trustly";
+                default:
+                    return method.name;
+            }
+        };
     }
 })();
