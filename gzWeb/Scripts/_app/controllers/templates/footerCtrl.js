fa28324b487f5d03166b3269be5fa7ea960b59b9
Manolis Marinos <manolis.marinos@outlook.com>
2016-09-21 15:57:01 +0300

    Highlight footer current page

--- gzWeb/Scripts/_app/controllers/templates/footerCtrl.js
+++ gzWeb/Scripts/_app/controllers/templates/footerCtrl.js
@@ -1,8 +1,21 @@
 ﻿(function () {
     'use strict';
     var ctrlId = 'footerCtrl';
-    APP.controller(ctrlId, ['$scope', 'localStorageService', 'constants', ctrlFactory]);
-    function ctrlFactory($scope, localStorageService, constants) {
+    APP.controller(ctrlId, ['$scope', 'localStorageService', 'constants', '$location', ctrlFactory]);
+    function ctrlFactory($scope, localStorageService, constants, $location) {
+        $scope.routes = [
+            constants.routes.transparency,
+            constants.routes.about,
+            constants.routes.faq,
+            constants.routes.help,
+            constants.routes.privacy,
+            constants.routes.terms,
+            constants.routes.promotions
+        ];
+        $scope.getClass = function (path) {
+            return $location.path() === path ? 'focus' : '';
+        }
+
         $scope.copyrightYears = (function () {
             var startingYear = 2016;
             var currentYear = moment().year();
