e96d4a3da868063c7ca1f534d5b53c1f245d4bce
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2016-09-22 21:00:30 +0300

    CHANGE: Auto increment build number

--- gzWeb/Properties/AssemblyInfo.cs
+++ gzWeb/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("gzWeb")]
+[assembly: AssemblyTitle("greenzorro website")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("gzWeb")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("greenzorro")]
+[assembly: AssemblyProduct("greenzorro site")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-//[assembly: AssemblyVersion("1.0.0.*")]
+
+// When in beta bump -> 0.9.x
+[assembly: AssemblyVersion("0.8.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
