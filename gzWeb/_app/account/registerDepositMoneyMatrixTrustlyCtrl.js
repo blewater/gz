03f84ffb5636c102693e372907a2d5ad2293c24f
Manolis Marinos <manolis.marinos@outlook.com>
2016-09-23 01:39:07 +0300

    Show deposit and withdraw limits (issue #95)

--- gzWeb/_app/account/registerDepositMoneyMatrixTrustlyCtrl.js
+++ gzWeb/_app/account/registerDepositMoneyMatrixTrustlyCtrl.js
@@ -10,8 +10,8 @@
         function loadCreditCardInfo() {
             $scope.gamingAccount = $scope.paymentMethodCfg.fields.gamingAccountID.options[0];
             $scope.currency = $scope.gamingAccount.currency;
-            $scope.amountPlaceholder = iso4217.getCurrencyByCode($scope.currency).symbol + " Amount";
             $scope.accountLimits = $scope.paymentMethodCfg.fields.amount.limits[$scope.currency];
+            $scope.amountPlaceholder = iso4217.getCurrencyByCode($scope.currency).symbol + " Amount (between " + $scope.accountLimits.min + " and " + $scope.accountLimits.max + ")";
         }
 
         function init() {
