c203b856528ef047d8d53f7ea9297e0d5f621cb8
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2016-09-26 13:20:31 +0300

    CHANGE: All Db Updates for game categories carousel

--- sql/LocalDb/dbo/Tables/EmailTemplates.sql
+++ sql/LocalDb/dbo/Tables/EmailTemplates.sql
@@ -3,8 +3,11 @@
     [Code]    NVARCHAR (100) NOT NULL,
     [Subject] NVARCHAR (MAX) NOT NULL,
     [Body]    NVARCHAR (MAX) NOT NULL,
+    [Updated] DATETIME       DEFAULT ('1900-01-01T00:00:00.000') NOT NULL,
     CONSTRAINT [PK_dbo.EmailTemplates] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 GO
