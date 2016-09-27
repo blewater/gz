770eb596d17da22cdc754a1bc7fef8c48cd73cbc
xdinos <xdinos@gmail.com>
2016-09-27 14:03:14 +0300

    Add DynamicPageTemplates and DynamicPageData entites. Keep in mind that we have to clear the data in DynamicPages table, so the migration script can work.

--- ModelDAL/Models/DynamicPage.cs
+++ ModelDAL/Models/DynamicPage.cs
@@ -10,6 +10,18 @@ using System.Web.Mvc;
 
 namespace gzDAL.Models
 {
+    public class DynamicPageTemplate
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required, AllowHtml]
+        public string Html { get; set; }
+    }
+
     public class DynamicPage
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -34,11 +46,32 @@ namespace gzDAL.Models
         [Required]
         public DateTime LiveTo { get; set; }
 
-        [Required]
-        [AllowHtml]
+        [Required, AllowHtml]
         public string Html { get; set; }
+
+        [Required, ForeignKey("DynamicPageTemplate")]
+        public int DynamicPageTemplateId { get; set; }
+        public DynamicPageTemplate DynamicPageTemplate { get; set; }
 
         [Required]
         public DateTime Updated { get; set; }
+    }
+    
+
+    public class DynamicPageData
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, ForeignKey("DynamicPage")]
+        public int DynamicPageId { get; set; }
+        public DynamicPage DynamicPage { get; set; }
+
+        [Required]
+        public string DataName { get; set; }
+        [Required]
+        public string DataType { get; set; }
+        [Required]
+        public string DataValue { get; set; }
     }
 }
