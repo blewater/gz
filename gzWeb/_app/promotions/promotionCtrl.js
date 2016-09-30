51ed414a834925f3dd7a3b523bc87680cbc510e8
Manolis Marinos <manolis.marinos@outlook.com>
2016-09-30 18:07:33 +0300

    Update promotions pages

--- gzWeb/_app/promotions/promotionCtrl.js
+++ gzWeb/_app/promotions/promotionCtrl.js
@@ -1,8 +1,8 @@
 ﻿(function () {
     'use strict';
     var ctrlId = 'promotionCtrl';
-    APP.controller(ctrlId, ['$scope', 'api', '$routeParams', ctrlFactory]);
-    function ctrlFactory($scope, api, $routeParams) {
+    APP.controller(ctrlId, ['$scope', 'api', '$routeParams', '$location', ctrlFactory]);
+    function ctrlFactory($scope, api, $routeParams, $location) {
         function loadPage() {
             var code = $routeParams.code;
             api.call(function () {
@@ -17,5 +17,9 @@
         };
 
         init();
+
+        $scope.go = function (path) {
+            $location.url(path);
+        };
     }
 })();
