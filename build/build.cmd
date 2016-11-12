c8d55398a5184b12b802df6f0a3bbd0a56f14a52
Mario Karagiorgas <salem8@gmail.com>
2016-11-12 12:34:37 +0200

    ADD: Fake greenzorro build script for develop/stage/production

---
+++ build/build.cmd
@@ -0,0 +1,22 @@
+﻿@echo off
+SETLOCAL
+
+cls
+
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+    exit /b %errorlevel%
+)
+
+.paket\paket.exe restore
+if errorlevel 1 (
+    exit /b %errorlevel%
+)
+
+SET FAKE_PATH=packages\FAKE\tools\Fake.exe
+
+IF [%1]==[] (
+    "%FAKE_PATH%" "build.fsx" "mode=prod" 
+) ELSE (
+    "%FAKE_PATH%" "build.fsx" %*
+)
