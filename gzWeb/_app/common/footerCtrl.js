a92e6793745ae1731d3d90ff1c46a583406003ff
Manolis Marinos <manolis.marinos@outlook.com>
2016-12-15 16:04:48 +0200

    1) Infrastucture for mga changes (#168), 2) Many html/css aesthetic changes (#163, #162)

--- gzWeb/_app/common/footerCtrl.js
+++ gzWeb/_app/common/footerCtrl.js
@@ -1,18 +1,18 @@
 ﻿(function () {
     'use strict';
     var ctrlId = 'footerCtrl';
-    APP.controller(ctrlId, ['$scope', 'localStorageService', 'constants', '$location', 'accountManagement', '$controller', ctrlFactory]);
-    function ctrlFactory($scope, localStorageService, constants, $location, accountManagement, $controller) {
+    APP.controller(ctrlId, ['$scope', 'localStorageService', 'constants', '$location', 'accountManagement', '$controller', '$rootScope', ctrlFactory]);
+    function ctrlFactory($scope, localStorageService, constants, $location, accountManagement, $controller, $rootScope) {
         $controller('authCtrl', { $scope: $scope });
 
-        $scope.routes = [
-            constants.routes.transparency,
-            constants.routes.about,
-            constants.routes.faq,
-            constants.routes.help,
-            constants.routes.privacy,
-            constants.routes.terms,
-            constants.routes.promotions
+        $scope.menu = [
+            { route: constants.routes.transparency, when: function () { return true; } },
+            { route: constants.routes.about, when: function () { return true; } },
+            { route: constants.routes.faq, when: function () { return $rootScope.routeData && $rootScope.routeData.investing; } },
+            { route: constants.routes.help, when: function () { return $rootScope.routeData && ($rootScope.routeData.gaming || $rootScope.routeData.wandering); } },
+            { route: constants.routes.privacy, when: function () { return $rootScope.routeData && ($rootScope.routeData.gaming || $rootScope.routeData.wandering); } },
+            { route: constants.routes.terms, when: function () { return true; } },
+            { route: constants.routes.promotions, when: function () { return true; } }
         ];
         $scope.getClass = function (path) {
             return $location.path() === path ? 'focus' : '';
