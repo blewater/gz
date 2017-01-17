1e0f6089a9298de37b96cc8252a57d15ec871bc0
Manolis Marinos <manolis.marinos@outlook.com>
2017-01-17 21:16:49 +0200

    Fix compliance feedback 27/01/2017 issues (#182)

--- gzWeb/_app/common/footerCtrl.js
+++ gzWeb/_app/common/footerCtrl.js
@@ -10,10 +10,26 @@
             { route: constants.routes.about, when: function () { return true; } },
             { route: constants.routes.faq, when: function () { return true; } },
             { route: constants.routes.help, when: function () { return true; } },
-            { route: constants.routes.privacyGames, when: function () { return $rootScope.routeData && ($rootScope.routeData.gaming || $rootScope.routeData.wandering); } },
-            { route: constants.routes.privacyInvestment, when: function () { return $rootScope.routeData && $rootScope.routeData.investing; } },
-            { route: constants.routes.termsGames, when: function () { return $rootScope.routeData && ($rootScope.routeData.gaming || $rootScope.routeData.wandering); } },
-            { route: constants.routes.termsInvestment, when: function () { return $rootScope.routeData && $rootScope.routeData.investing; } },
+            {
+                title: "Privacy Policy",
+                subroutes: [
+                    { route: constants.routes.privacyGames, when: function () { return $rootScope.routeData && ($rootScope.routeData.gaming || $rootScope.routeData.wandering); } },
+                    { route: constants.routes.privacyInvestment, when: function () { return $rootScope.routeData && $rootScope.routeData.investing; } },
+                ],
+                when: function () { return true; }
+            },
+            {
+                title: "Terms & Conditions",
+                subroutes: [
+                    { route: constants.routes.termsGames, when: function () { return $rootScope.routeData && ($rootScope.routeData.gaming || $rootScope.routeData.wandering); } },
+                    { route: constants.routes.termsInvestment, when: function () { return $rootScope.routeData && $rootScope.routeData.investing; } }
+                ],
+                when: function () { return true; }
+            },
+            //{ route: constants.routes.privacyGames, when: function () { return $rootScope.routeData && ($rootScope.routeData.gaming || $rootScope.routeData.wandering); } },
+            //{ route: constants.routes.privacyInvestment, when: function () { return $rootScope.routeData && $rootScope.routeData.investing; } },
+            //{ route: constants.routes.termsGames, when: function () { return $rootScope.routeData && ($rootScope.routeData.gaming || $rootScope.routeData.wandering); } },
+            //{ route: constants.routes.termsInvestment, when: function () { return $rootScope.routeData && $rootScope.routeData.investing; } },
             { route: constants.routes.promotions, when: function () { return true; } }
         ];
         $scope.getClass = function (path) {
