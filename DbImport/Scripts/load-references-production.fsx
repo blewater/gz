50b1d63dda14727f4878f088e9e857a074945aad
Mario Karagiorgas <salem8@gmail.com>
2017-01-20 12:08:36 +0200

    CHANGE: Custom dbSet functions

---
+++ DbImport/Scripts/load-references-production.fsx
@@ -0,0 +1,15 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../../GmTrx/packages/ExcelProvider.0.8.0/lib/Excel.dll"
+#r "../../GmTrx/packages/ExcelProvider.0.8.0/lib/ExcelProvider.dll"
+#r "../../GmTrx/packages/FSharp.Data.2.3.2/lib/net40/FSharp.Data.dll"
+#r "../../GmTrx/packages/FSharp.Data.TypeProviders.5.0.0.2/lib/net40/FSharp.Data.TypeProviders.dll"
+#r "../../GmTrx/packages/ExcelProvider.0.8.0/lib/ICSharpCode.SharpZipLib.dll"
+#r "../../GmTrx/packages/NLog.4.3.9/lib/net45/NLog.dll"
+#r "System.Core.dll"
+#r "System.Data.dll"
+#r "System.Data.Linq.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
+#r "System.Transactions.dll"
+#r "System.Xml.Linq.dll"
