0a275a9f9a6c525313e1e009ca3deb10bb93747b
Mario Karagiorgas <salem8@gmail.com>
2017-01-25 12:58:26 +0200

    CHANGE: Sync production Sql db with others, stop tracking visual studio *.jfm files

--- sql/Prod/dbo/Tables/PlayerRevRpt.sql
+++ sql/Prod/dbo/Tables/PlayerRevRpt.sql
@@ -1,12 +1,11 @@
 ﻿CREATE TABLE [dbo].[PlayerRevRpt] (
-    [Id]                                   INT              IDENTITY (1, 1) NOT NULL,
+    [ID]                                   INT              IDENTITY (1, 1) NOT NULL,
     [User ID]                              INT              NOT NULL,
     [Username]                             NVARCHAR (256)   NOT NULL,
     [YearMonth]                            CHAR (6)         NOT NULL,
     [YearMonthDay]                         CHAR (8)         NOT NULL,
     [BegBalance]                           DECIMAL (29, 16) NULL,
     [EndBalance]                           DECIMAL (29, 16) NULL,
-    [PlayerLoss]                           DECIMAL (29, 16) NULL,
     [Role]                                 NVARCHAR (256)   NULL,
     [Player status]                        NVARCHAR (256)   NULL,
     [Block reason]                         NVARCHAR (256)   NULL,
@@ -16,7 +15,6 @@
     [Total deposits amount]                DECIMAL (29, 16) NULL,
     [Withdraws made]                       DECIMAL (29, 16) NULL,
     [Last played date]                     DATE             NULL,
-    [Deposits made]                        INT              NULL,
     [Currency]                             CHAR (3)         NOT NULL,
     [Total bonuses accepted by the player] DECIMAL (29, 16) NULL,
     [Net revenue]                          DECIMAL (29, 16) NULL,
@@ -27,9 +25,11 @@
     [CreatedOnUtc]                         DATETIME         NOT NULL,
     [UpdatedOnUtc]                         DATETIME         NOT NULL,
     [PendingWithdrawals]                   DECIMAL (29, 16) NULL,
-    [TotalWithdrawals]                     DECIMAL (29, 16) NULL,
-    CONSTRAINT [PK_dbo.PlayerRevRpt] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [PlayerGainLoss]                       DECIMAL (29, 16) NULL,
+    CONSTRAINT [PK_dbo.PlayerRevRpt] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
