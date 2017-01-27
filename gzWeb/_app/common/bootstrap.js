38fbb1090667efc30cc54175e29c5de6825cdd8a
Manolis Marinos <manolis.marinos@outlook.com>
2017-01-27 17:16:21 +0200

    Improve PageSpeed Insights ranking (3rd attempt)

--- gzWeb/_app/common/bootstrap.js
+++ gzWeb/_app/common/bootstrap.js
@@ -1,7 +1,17 @@
 (function () {
     'use strict';
 
+    function hidePreloader() {
+        var preloader = document.getElementById("preloader");
+        preloader.className = "die";
+        setTimeout(function () {
+            var body = document.getElementsByTagName("BODY")[0];
+            body.removeChild(preloader);
+        }, 1000);
+    };
+
     angular.element(document).ready(function () {
         angular.bootstrap(document, [APP.id]);
+        hidePreloader();
     });
 })();
