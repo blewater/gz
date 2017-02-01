febffebde5a69400886ec45a8fcce00c9af86b56
Mario Karagiorgas <salem8@gmail.com>
2017-02-01 10:08:47 +0200

    CHANGE: Remove auto generated changes fsx tracking

--- GzBalances/Scripts/load-references-production.fsx
+++
@@ -1,7 +0,0 @@
-// Warning: generated file; your changes could be lost when a new file is generated.
-#I __SOURCE_DIRECTORY__
-#r "System.Core.dll"
-#r "System.dll"
-#r "System.Numerics.dll"
-#r "../../GzDb/bin/Production/GzDb.dll"
-#r "System.Data.Linq.dll"
