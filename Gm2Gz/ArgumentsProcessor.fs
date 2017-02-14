7dca7e5a12aed06bceeab0bb9846104cf83a74a1
Mario Karagiorgas <salem8@gmail.com>
2017-02-14 02:19:17 +0200

    ADD: Multiple days of portfolio shares calculating

--- Gm2Gz/ArgumentsProcessor.fs
+++ Gm2Gz/ArgumentsProcessor.fs
@@ -2,42 +2,36 @@
 
 open Argu
 
-type HandleShares =
-    | Store of int
-    | Skip
-    | OneDay
+    /// Any value of other than 0 or 1 proceeds with the remaining processing
+    type HandleShares =
+        | Store of int // 0 or 1 only
+        | StoreOnlyAction of int // Values of >= 2
 
-type Gm2GzArguments =
-    | [<First>] Store_Shares of days:int
-    | [<First>] Skip_Shares
-with
-    interface IArgParserTemplate with
-        member s.Usage =
-            match s with
-                | Store_Shares _ -> "specify number of days to store shares 1 - 300."
-                | Skip_Shares _ -> "specify number of days to store shares 1 - 300."
+    type Gm2GzArguments =
+        | Store_Shares of days:int
+    with
+        interface IArgParserTemplate with
+            member s.Usage =
+                match s with
+                | Store_Shares _ -> "specify number of days to store shares 0 - 300."
     
-
-let parseCmdArgs (argv : string[]) : HandleShares=
-
-    let parseDays (d :int)  = 
-        if d < 1 || d > 300 then 
-            failwith (sprintf "invalid days [1-300] number. Instead given %d." d)
-        else d
-
-    let getDays =
-        function 
-        | Some d -> d
-        | None -> 1
-
-    let parser = ArgumentParser.Create<Gm2GzArguments>(programName = "Gm2Gz.exe")
-    let results = parser.Parse(argv)
-    let skipShares = results.Contains(<@ Skip_Shares @>)
-    if skipShares then
-        Skip
-    else 
-        Store(
+    let parseCmdArgs (argv : string[]) : HandleShares=
+        let getDays =
+            function
+            | Some d -> d
+            | None -> 1
+        let parseDays (d :int)  = 
+            if d < 0 || d > 300 then 
+                failwith (sprintf "invalid days [1-300] number. Instead given %d." d)
+            else d
+        let parser = ArgumentParser.Create<Gm2GzArguments>(programName = "Gm2Gz.exe")
+        let results = parser.Parse(argv)
+        if results.Contains(<@ Store_Shares @>) then
             results.TryGetResult(<@ Store_Shares @>) 
-            |> getDays 
+            |> getDays
             |> parseDays
-        )
+            |> function
+                | 0 -> Store(0)
+                | 1 -> Store(1)
+                | days -> StoreOnlyAction(days)
+        else Store(1)
