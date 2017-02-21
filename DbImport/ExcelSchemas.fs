8fe7b98c6aa22bd0c2f06274e85b88cd4e3b6e27
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2017-02-21 11:17:43 +0200

    ADD: logging to GzBalances CHANGE: upgrade f# core, pendingwithdraw with yyyyMMdd filemask

--- DbImport/ExcelSchemas.fs
+++ DbImport/ExcelSchemas.fs
@@ -8,7 +8,7 @@ module ExcelSchemas =
     // Compile type
     type BalanceExcelSchema = ExcelFile< "Balance Prod 20161001.xlsx" >
     type CustomExcelSchema = ExcelFile< "Custom Prod 20160930.xlsx" >
-    type WithdrawalsExcelSchema = ExcelFile< "pendingwithdraw prod 201609.xlsx" >
+    type WithdrawalsExcelSchema = ExcelFile< "pendingwithdraw prod 20160930.xlsx" >
 
     type RptType =
     | CustomRptExcel
