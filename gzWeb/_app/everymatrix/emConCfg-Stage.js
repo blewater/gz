6ee98f1ad859a8cb82277498ae199dc63a5ae181
Manolis Marinos <manolis.marinos@outlook.com>
2017-05-15 16:29:31 +0300

    Update ts2 stage urls

--- gzWeb/_app/everymatrix/emConCfg-Stage.js
+++ gzWeb/_app/everymatrix/emConCfg-Stage.js
@@ -2,8 +2,8 @@
     'use strict';
 
     APP.constant("emConCfg", {
-        webSocketApiUrl: 'wss://webapi-stage.everymatrix.com/v2',//'wss://api-stage.everymatrix.com/v2',
-        fallbackApiUrl: 'https://fb-webapi-stage.everymatrix.com',//'https://api-stage.everymatrix.com/longpoll',
+        webSocketApiUrl: 'wss://api-stage.everymatrix.com/v2',//'wss://webapi-stage.everymatrix.com/v2',
+        fallbackApiUrl: 'https://api-stage.everymatrix.com/longpoll',//'https://fb-webapi-stage.everymatrix.com',
         domainPrefix: 'http://www.greenzorro.com'
     });
 })();
