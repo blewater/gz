d189e3fbd3eb18b019fd89a25ee59468c82913ee
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2017-06-04 23:02:10 +0300

    ADD, city, phone, country,mobile custom columns

--- DbImport/ExcelSchemas.fs
+++ DbImport/ExcelSchemas.fs
@@ -7,7 +7,7 @@ module ExcelSchemas =
 
     // LGA Reports...Player Balances
     type BalanceExcelSchema = ExcelFile< "Balance Prod 20161001.xlsx" >
-    type CustomExcelSchema = ExcelFile< "Custom Prod 20170505.xlsx" >
+    type CustomExcelSchema = ExcelFile< "Custom Prod 20170601.xlsx" >
     // Transactions Rpt: Withdrawal Type + Initiated + Pending + Rollback
     type WithdrawalsPendingExcelSchema = ExcelFile< "WithdrawalsPending prod 20160930.xlsx" >
     // Transactions Rpt: Withdrawal Type + Completed + Rollback
