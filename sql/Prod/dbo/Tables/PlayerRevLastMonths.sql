12e3f9b09a8524fa2330c7cd794780a59937a1eb
Mario Karagiorgas <salem8@gmail.com>
2017-08-04 17:39:25 +0300

    ADD: SQL playerrevrpt not null currency misc....

---
+++ sql/Prod/dbo/Tables/PlayerRevLastMonths.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[PlayerRevLastMonths] (
+    [Id]           INT      DEFAULT ((1)) NOT NULL,
+    [YearMonth]    CHAR (6) NOT NULL,
+    [UpdatedOnUtc] DATETIME NOT NULL,
+    CONSTRAINT [PK_dbo.PlayerRevLastMonths] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
