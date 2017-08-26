514990ed99bd29eb6d6a3ce690bee8207c794792
Manolis Marinos <manolis.marinos@outlook.com>
2017-08-26 19:03:08 +0300

    1) Refactor withdraw implementations (#217, #256, #257, #260), 2) Clear messages upon successful login

--- gzWeb/_app/accountManagement/withdrawMoneyMatrixCreditCardCtrl.js
+++ gzWeb/_app/accountManagement/withdrawMoneyMatrixCreditCardCtrl.js
@@ -1,47 +1,14 @@
 ﻿(function () {
     'use strict';
     var ctrlId = 'withdrawMoneyMatrixCreditCardCtrl';
-    APP.controller(ctrlId, ['$scope', '$q', 'iso4217', 'auth', '$filter', ctrlFactory]);
-    function ctrlFactory($scope, $q, iso4217, auth, $filter) {
-        $scope.model = {
-            amount: undefined,
-        };
+    APP.controller(ctrlId, ['$scope', '$q', 'iso4217', 'auth', '$filter', '$controller', ctrlFactory]);
+    function ctrlFactory($scope, $q, iso4217, auth, $filter, $controller) {
+        $controller('withdrawMoneyMatrixCommonCtrl', { $scope: $scope });
 
-        function loadCreditCardInfo() {
-            $scope.payCardID = $scope.paymentMethodCfg.fields.payCardID.options[0];
-            $scope.gamingAccount = $scope.paymentMethodCfg.fields.gamingAccountID.options[0];
-            $scope.currency = $scope.gamingAccount.currency;
-            $scope.accountLimits = $scope.paymentMethodCfg.fields.amount.limits[$scope.currency];
-            $scope.accountLimitMax = Math.min($scope.accountLimits.max, auth.data.gamingAccounts[0].amount);
-            //$scope.canWithdraw = $scope.accountLimits.min <= $scope.accountLimitMax;
-            $scope.limitMin = 1;//$scope.accountLimits.min;
-            $scope.limitMax = $scope.accountLimitMax;
-            $scope.amountPlaceholder = iso4217.getCurrencyByCode($scope.currency).symbol + " Amount (between " + $filter('number')($scope.limitMin, 2) + " and " + $filter('number')($scope.limitMax, 2) + ")";
-        }
-
-        function init() {
-            loadCreditCardInfo();
-        };
-
-        function getFields() {
-            return {
-                gamingAccountID: $scope.gamingAccount.id,
-                currency: $scope.currency,
-                amount: $scope.model.amount,
-                payCardID: $scope.payCardID.id,
-            };
-        }
-
-        $scope.readFields = function () {
-            var q = $q.defer();
-            q.resolve(getFields());
-            return q.promise;
-        }
-
-        $scope.readConfirmMessage = function (prepareData) {
+        $scope.getSpecificConfirmMessage = function (prepareData) {
             return "Do you want to withdraw the amount of " + prepareData.debitAmount + " to " + prepareData.creditTo + "?";
         };
 
-        init();
+        $scope._init();
     }
 })();
