039ee84c933d7fb6e9a0d3f6ffc43765e803c646
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2017-09-04 02:53:09 +0300

    CHANGE: consistent server cache, put autobahn back to bundle

--- gzWeb/App_Start/BundleConfig.cs
+++ gzWeb/App_Start/BundleConfig.cs
@@ -78,7 +78,7 @@ namespace gzWeb {
             bundles.Add(new ScriptBundle("~/js/plugins").Include(
                 "~/Scripts/moment/moment.min.js"
                 , "~/Scripts/spin/spin.min.js"
-                //, "~/Scripts/autobahn/autobahn.min.js"
+                , "~/Scripts/autobahn/autobahn.min.js"
             ));
 
             bundles.Add(new ScriptBundle("~/js/angular").Include(
