f5b8bc503c7563290a5828ac2fa2c6d49e99098d
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2017-09-26 17:18:33 +0300

    CHANGE: Safe Partial Merge of CdnOpt changes

--- gzWeb/Controllers/AccountApiController.cs
+++ gzWeb/Controllers/AccountApiController.cs
@@ -712,7 +712,8 @@ namespace gzWeb.Controllers
         [AllowAnonymous]
         [HttpGet]
         [Route("GetDeploymentInfo")]
-        [System.Web.Mvc.OutputCache(Duration = 86400, VaryByParam = "none", NoStore = false)]
+        // 10 minutes caching
+        [System.Web.Mvc.OutputCache(Duration = 6000, VaryByParam = "none", NoStore = false)]
         public IHttpActionResult GetDeploymentInfo()
         {
             return OkMsg(new
