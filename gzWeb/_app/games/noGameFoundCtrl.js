8a7a87f361dffbff9eaa2c4c647a2514186a0f6f
Manolis Marinos <manolis.marinos@outlook.com>
2017-11-01 22:19:29 +0200

    Check if there is no game with the requested slug

---
+++ gzWeb/_app/games/noGameFoundCtrl.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+    var ctrlId = 'noGameFoundCtrl';
+    APP.controller(ctrlId, ['$scope', '$location', 'constants', ctrlFactory]);
+    function ctrlFactory($scope, $location, constants) {
+        $scope.backToGames = function () {
+            $scope.nsOk(true);
+            //$location.path(constants.routes.games.path).search($scope.searchParams);
+        }
+    }
+})();
