3145a705b0a155dd1b3c451b16061a7244fef79b
Mario Karagiorgas <salem8@gmail.com>
2018-01-23 20:12:10 +0200

    CHANGE: Move everything to top gz folder; az functions won't be used

--- gzFunctions/AwardVintageBonus/run.fsx
+++
@@ -1,6 +0,0 @@
-open AngleSharp;
-open AngleSharp.Dom.Html;
-open AngleSharp.Extensions;
-
-let Run(inputMessage: string, log: TraceWriter) =
-    log.Info(sprintf "F# Queue trigger function processed: '%s'" inputMessage)
