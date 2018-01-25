f38779d060cd9ecacaacf65b856402e4bcdebd0d
Mario Karagiorgas <salem8@gmail.com>
2018-01-25 17:54:09 +0200

    ADD: because of interruption

--- awardvintagebonus/Queue.fsx
+++ awardvintagebonus/Queue.fsx
@@ -3,3 +3,7 @@
 #r "./packages/FSharp.Data.TypeProviders/lib/net40/FSharp.Data.TypeProviders.dll"
 #r "./packages/NLog/lib/net45/NLog.dll"
 #r "./packages/WindowsAzure.Storage/lib/net45/Microsoft.WindowsAzure.Storage.dll"
+#r "./packages/FSharp.Configuration\lib/net45/FSharp.Configuration.dll"
+open FSharp.Configuration
+
+type Settings = AppSettings<"App.config">
