d2772af37e9112975cda8d49c4ce3901ca700e97
Manolis Marinos <manolis.marinos@outlook.com>
2018-02-02 10:18:32 +0200

    Fix bug with invalid paycard id on withdrawals (issue #326)

--- gzWeb/_app/accountManagement/withdrawMoneyMatrixTrustlyCtrl.js
+++ gzWeb/_app/accountManagement/withdrawMoneyMatrixTrustlyCtrl.js
@@ -5,6 +5,9 @@
     function ctrlFactory($scope, $q, iso4217, $filter, $controller) {
         $controller('withdrawMoneyMatrixCommonCtrl', { $scope: $scope });
 
+        if ($scope.selected.group.length === 1)
+            $scope.selected.method = $scope.selected.group[0];
+
         $scope._init();
     }
 })();
