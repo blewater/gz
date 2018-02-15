c1427e83991290777d695e27c04337ed386c058a
Mario Karagiorgas <salem8@gmail.com>
2018-02-15 19:43:14 +0200

    ADD: InvAdj table

---
+++ sql/Prod/dbo/Tables/InvAdjs.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[InvAdjs] (
+    [Id]           INT              IDENTITY (1, 1) NOT NULL,
+    [YearMonth]    CHAR (6)         NOT NULL,
+    [UserId]       INT              NOT NULL,
+    [AmountType]   INT              NOT NULL,
+    [Amount]       DECIMAL (29, 16) NOT NULL,
+    [UpdatedOnUtc] DATETIME         NOT NULL,
+    CONSTRAINT [PK_dbo.InvAdjs] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_dbo.InvAdjs_dbo.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [Inv_Adjustment_Idx]
+    ON [dbo].[InvAdjs]([YearMonth] ASC, [UserId] ASC);
+
