47fa1d7a6c4c4e2a34af246d4636cb91e1674d93
Mario Karagiorgas <salem8@gmail.com>
2018-02-21 17:28:49 +0200

    ADD: emailing

---
+++ awardvintagebonus/SendEmail.fs
@@ -0,0 +1,70 @@
+﻿#if INTERACTIVE
+#r "./packages/MailKit/lib/net45/MailKit.dll"
+#r "./packages/MimeKit/lib/net45/MimeKit.dll"
+#else
+module SendEmail
+#endif
+
+open MimeKit
+open NLog
+open MailKit.Net.Smtp
+open MailKit.Security
+
+type EmailAccess() =
+
+    static let logger = LogManager.GetCurrentClassLogger()
+
+    member this.SendBonusReqAdminReceipt(fromGmailUser: string)(fromGmailPwd : string)(gmUserId : int)(userEmail : string)(amount: string) =
+
+        use smtpClient = new SmtpClient()
+        smtpClient.Connect("smtp.gmail.com", 465, SecureSocketOptions.SslOnConnect)
+        smtpClient.Authenticate(fromGmailUser, fromGmailPwd)
+
+        let msg = MimeMessage()
+        msg.From.Add(MailboxAddress ("Admin", "admin@greenzorro.com"))
+        //msg.To.Add(MailboxAddress ("Antonis", "antonis@greenzorro.com"))
+        msg.To.Add(MailboxAddress ("Mario", "mario@greenzorro.com"))
+        msg.Subject <- sprintf "Bonus fulfilled for %s" <| userEmail
+        let body = TextPart ("plain")
+        body.Text <- 
+            sprintf "Bonus fulfilled for\n\
+                    user id:%d\n\
+                    email: %s\n\
+                    Amount: %s.\n\
+                    \n\
+                    Cheers\n\
+                    \n\
+                    Your friendly admin process."<| gmUserId <| userEmail <| amount
+
+        msg.Body <- body
+
+        smtpClient.Send(msg)
+        smtpClient.Disconnect(true)
+
+    member this.SendBonusReqUserReceipt(fromGmailUser: string)(fromGmailPwd : string)(userEmail : string)(firstName : string)(amount: string) =
+
+        use smtpClient = new SmtpClient()
+        smtpClient.Connect("smtp.gmail.com", 465, SecureSocketOptions.SslOnConnect)
+        smtpClient.Authenticate(fromGmailUser, fromGmailPwd)
+
+        let msg = MimeMessage()
+        msg.From.Add(MailboxAddress ("Help", "help@greenzorro.com"))
+        msg.To.Add(MailboxAddress (firstName, userEmail))
+        msg.Subject <- sprintf "greenzorro fullfilled your Bonus request"
+        let body = TextPart ("plain")
+        body.Text <- 
+            sprintf "Dear %s,\n\
+                    \n\
+                    Further to your request, we would like to inform you that your Investment bonus of %s has been credited to your account!\n\
+                    \n\
+                    In order to turn your bonus into cash, all that is required is to place a single minimum bet on one of our games (even 0,10 EUR).\n\
+                    \n\
+                    Thank you for choosing Greenzorro and we wish you a wonderful day and all the best for the new year!\n\
+                    \n\
+                    Best regards,\n\
+                    \n\
+                    Greenzorro Support Team" <| firstName <| amount
+        msg.Body <- body
+
+        smtpClient.Send(msg)
+        smtpClient.Disconnect(true)
