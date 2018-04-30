dcc2a9526cd723b9dffdf1e5d3c4ca9c84956b4f
Mario Karagiorgas <salem8@gmail.com>
2018-04-30 14:24:46 +0300

    ADD: GzBatch latest package update and new Emailing lib

---
+++ Emailing/Segmentation.fs
@@ -0,0 +1,13 @@
+﻿module Segmentation
+
+open System
+open FSharp.Azure.StorageTypeProvider
+open Microsoft.WindowsAzure.Storage
+open FSharp.Azure.StorageTypeProvider.Table
+open Newtonsoft.Json
+open NLog
+
+let logger = LogManager.GetCurrentClassLogger()
+
+// Connect via configuration file with named connection string.
+type AzStorage = AzureTypeProvider<"DefaultEndpointsProtocol=https;AccountName=gzazurefunctionsstorage;AccountKey=70GYx9psHLDTh+y4TBIYNb2OSkXRWy5JTIdFFGL7GY3EsrTnebs8ztsyEu3ro5ZEnOm1g8RmgbrlORpSQuJEjQ==;EndpointSuffix=core.windows.net", tableSchema="Registrations.json">
