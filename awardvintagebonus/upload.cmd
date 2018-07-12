0a932f538100d1fbe70888bf3fee5f7175602933
Mario Karagiorgas <mario.karagiorgas@greenzorro.com>
2018-07-12 12:01:05 +0300

    FIX: login to azure container registry

--- awardvintagebonus/upload.cmd
+++ awardvintagebonus/upload.cmd
@@ -1,4 +1,4 @@
-docker login gzcontainerregistry.azurecr.iodocker -u gzContainerRegistry -p P7QqmKxsMPUyrPtB7gAT6lM94+7VUoRC
+docker login gzcontainerregistry.azurecr.io -u gzContainerRegistry -p P7QqmKxsMPUyrPtB7gAT6lM94+7VUoRC
 docker tag awardbonus gzcontainerregistry.azurecr.io/awardbonus:v%1
 docker push gzcontainerregistry.azurecr.io/awardbonus:v%1
 az acr repository show-tags --name gzContainerRegistry --repository awardbonus --output table
